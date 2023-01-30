a05e3d5e75c761d781c762c764c610799cce5770
Sena543 <senanuazanu@gmail.com>
2023-01-30 13:12:15 +0000

    file: Root layout file

---
+++ src/layouts/Root.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Sidebar from "./Sidebar/Sidebar";
+
+function Root() {
+	return <></>;
+}
+
+export default Root;
