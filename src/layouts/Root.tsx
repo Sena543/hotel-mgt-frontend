e02274b0028e1dd6488dd373f7016be93f6e026a
Sena543 <senanuazanu@gmail.com>
2023-01-30 14:25:44 +0000

    remove: removed unused imports

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar/Sidebar";
 
 function Root() {
 	return (
 		<>
 			<Sidebar />
+			<Outlet />
 		</>
 	);
 }
