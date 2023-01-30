a16069468f34f755d20c886125525ddd968b0b06
Sena543 <senanuazanu@gmail.com>
2023-01-30 13:46:06 +0000

    add: nav links and paths

--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -1,15 +1,24 @@
 import { Drawer } from "@mui/material";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import React from "react";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
-	const sidebarNavLinks = ["Dashboard", "Room", "Booking", "Guests", "Concierge", "Staff"];
+	const sidebarNavLinks = [
+		{ name: "Dashboard", link: "/" },
+		{ name: "Room", link: "rooms" },
+		{ name: "Booking", link: "booking" },
+		{ name: "Guests", link: "guests" },
+		{ name: "Concierge", link: "concierge" },
+		{ name: "Staff", link: "employees" },
+	];
 	return (
 		<Drawer variant="persistent" anchor="left" open={true}>
 			<List>
-				{sidebarNavLinks.map((link) => (
-					<ListItem>{link}</ListItem>
+				{sidebarNavLinks.map(({ name, link }) => (
+					<ListItem key={link}>
+						<Link to={link}>{name}</Link>
+					</ListItem>
 				))}
 			</List>
 		</Drawer>
