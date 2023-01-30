10d8fbef32e1b8030030514fb13efdebf16d32cc
Sena543 <senanuazanu@gmail.com>
2023-01-30 13:14:37 +0000

    add: setup sidebar with list items

--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -1,7 +1,19 @@
+import { Drawer } from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 import React from "react";
 
 function Sidebar() {
-	return <div></div>;
+	const sidebarNavLinks = ["Dashboard", "Room", "Booking", "Guests", "Concierge", "Staff"];
+	return (
+		<Drawer variant="persistent" anchor="left" open={true}>
+			<List>
+				{sidebarNavLinks.map((link) => (
+					<ListItem>{link}</ListItem>
+				))}
+			</List>
+		</Drawer>
+	);
 }
 
 export default Sidebar;
