135bcd2ece2fee33daa6fcbcf274ab3e6b7b6065
Sena543 <senanuazanu@gmail.com>
2023-01-31 16:33:48 +0000

    room header component

--- src/components/Room/RoomHeader.tsx
+++ src/components/Room/RoomHeader.tsx
@@ -1,8 +1,17 @@
-import { Paper } from "@mui/material";
+import "./roomHeader.css";
+import { Paper, Typography } from "@mui/material";
 
 function RoomHeader() {
-    const roomAvailabilityTypes = []
-	return <Paper>RoomHeader</Paper>;
+	const roomAvailabilityTypes = ["All Rooms (50)", "Available Rooms(30)", "Booked Rooms(20)"];
+	return (
+		<Paper className="room-header-paper">
+			{roomAvailabilityTypes.map((room) => (
+				<div className="roomHeader">
+					<Typography variant="h6">{room}</Typography>
+				</div>
+			))}
+		</Paper>
+	);
 }
 
 export default RoomHeader;
