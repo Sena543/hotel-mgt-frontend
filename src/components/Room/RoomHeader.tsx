e134d65baa7fa29f20e4eb93c7fcb13f4991f695
Sena543 <senanuazanu@gmail.com>
2023-02-01 13:59:00 +0000

    add: room header almost finished

--- src/components/Room/RoomHeader.tsx
+++ src/components/Room/RoomHeader.tsx
@@ -1,16 +1,38 @@
 import "./roomHeader.css";
-import { Paper, Typography } from "@mui/material";
+import { Paper, Typography, InputAdornment } from "@mui/material";
+import CustomTextField from "../TextInput/CustomTextField";
+import { SearchRounded } from "@mui/icons-material";
 
 function RoomHeader() {
 	const roomAvailabilityTypes = ["All Rooms (50)", "Available Rooms(30)", "Booked Rooms(20)"];
 	return (
-		<Paper className="room-header-paper">
-			{roomAvailabilityTypes.map((room) => (
-				<div className="roomHeader">
-					<Typography variant="h6">{room}</Typography>
-				</div>
-			))}
-		</Paper>
+		<div className="room-header-container">
+			<Paper className="room-header-paper">
+				{roomAvailabilityTypes.map((room) => (
+					<div className="roomHeader">
+						<Typography variant="h6">{room}</Typography>
+					</div>
+				))}
+			</Paper>
+			<div className="textfield-div">
+				<CustomTextField
+					fullWidth
+					placeholder="Search here..."
+					// style={{ marginLeft: 20 }}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="end" style={{ marginBottom: 15 }}>
+								<SearchRounded />
+							</InputAdornment>
+						),
+					}}
+				/>
+			</div>
+			<div className="schedule-div">
+				{/* <input id="dateInput" type="date" /> */}
+				<Typography variant="h6">Check schedule</Typography>
+			</div>
+		</div>
 	);
 }
 
