09409f0c599232882fa2e71b011f95d96cae2575
Sena543 <senanuazanu@gmail.com>
2023-02-01 16:12:32 +0000

    glance finished on dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { BookmarkBorderRounded, LogoutRounded, LoginRounded, CalendarMonthRounded } from "@mui/icons-material";
+
+import { GlanceCards } from "../../components/dashboard/GlanceCards";
 
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+	const glanceData = [
+		{ name: "New Booking", number: 123, icon: <BookmarkBorderRounded fontSize="large" />, backgColor: "#c5b5d0" },
+		{ name: "Schedule Room", number: 2, icon: <CalendarMonthRounded fontSize="large" />, backgColor: "#a5ecca" },
+		{ name: "Check in", number: 3, icon: <LoginRounded fontSize="large" />, backgColor: "#f9d4a8" },
+		{ name: "Check out", number: 5, icon: <LogoutRounded fontSize="large" />, backgColor: "#fcddda" },
+	];
+	return (
+		<div>
+			<div style={{ display: "flex", flexDirection: "row" }}>
+				{glanceData.map(({ name, number, icon, backgColor }) => (
+					<GlanceCards name={name} number={number} icon={icon} backgColor={backgColor} />
+				))}
+			</div>
+		</div>
+	);
 }
 
-export default Dashboard
+export default Dashboard;
