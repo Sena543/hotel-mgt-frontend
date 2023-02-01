b96440234614ec6a39e05efc354cd57c1bfb31bc
Sena543 <senanuazanu@gmail.com>
2023-02-01 14:00:30 +0000

    .

--- src/pages/Room/Rooms.tsx
+++ src/pages/Room/Rooms.tsx
@@ -1,9 +1,14 @@
 import { Container } from "@mui/material";
+import RoomHeader from "../../components/Room/RoomHeader";
+import RoomList from "../../components/Room/RoomList";
 
 function Rooms() {
-	return <Container>
-		
-	</Container>;
+	return (
+		<Container className="room-container">
+			<RoomHeader />
+			<RoomList />
+		</Container>
+	);
 }
 
 export default Rooms;
