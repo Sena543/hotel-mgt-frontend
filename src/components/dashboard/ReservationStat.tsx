a68ca42b3ec4ffc5781a221d8876f9be6c291fdd
Sena543 <senanuazanu@gmail.com>
2023-02-02 11:54:57 +0000

    one chart done

--- src/components/dashboard/ReservationStat.tsx
+++ src/components/dashboard/ReservationStat.tsx
@@ -1,7 +1,24 @@
 import GenericDashCards from "../Cards/GenericDashCards";
+import { ResponsiveStream } from "@nivo/stream";
+import { lineGraphData } from "../../services/lineGraphData";
 
 function ReservationStat() {
-	return <GenericDashCards>ReservationStat</GenericDashCards>;
+	return (
+		<GenericDashCards>
+			<div style={{ width: "100%", height: "96%", display: "flex" }}>
+				<ResponsiveStream
+					data={lineGraphData}
+					keys={["Check in", "Check out"]}
+					margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+					axisTop={null}
+					axisRight={null}
+					enableGridX={true}
+					enableGridY={false}
+					fillOpacity={0.85}
+				/>
+			</div>
+		</GenericDashCards>
+	);
 }
 
 export default ReservationStat;
---
+++ src/services/lineGraphData.ts
@@ -0,0 +1,22 @@
+export const lineGraphData = [
+	{
+		"Check in": 20,
+		"Check out": 10,
+	},
+	{
+		"Check in": 5,
+		"Check out": 2,
+	},
+	{
+		"Check in": 7,
+		"Check out": 5,
+	},
+	{
+		"Check in": 2,
+		"Check out": 1,
+	},
+	{
+		"Check in": 15,
+		"Check out": 2,
+	},
+];
