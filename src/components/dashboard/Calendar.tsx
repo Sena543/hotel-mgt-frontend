7ffefd52b9c54889a029974059ac4e2e2f9c9dfe
Sena543 <senanuazanu@gmail.com>
2023-02-02 09:28:40 +0000

    add: placeholder calendar card rendered on generic card

---
+++ src/components/dashboard/Calendar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import GenericDashCards from "../Cards/GenericDashCards";
+
+function Calendar() {
+	return <GenericDashCards>Calendar</GenericDashCards>;
+}
+
+export default Calendar;
