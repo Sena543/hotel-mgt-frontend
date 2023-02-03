02ba602e7af9ef449a47b6be05f305a6640b9bf7
Sena543 <senanuazanu@gmail.com>
2023-02-03 12:53:12 +0000

    hardcoded data for various ui

---
+++ src/services/guests.ts
@@ -0,0 +1,83 @@
+export const guests = [
+	{
+		name: "Clark Kent",
+		room: "G-01",
+		checkIn: "12-01-2023",
+		checkOut: "20-02-2023",
+		request:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis a condimentum vitae sapien pellentesque. Mauris pharetra et ultrices neque ornare aenean euismod elementum. ",
+		status: "Booked",
+	},
+	{
+		name: "Bruce Wayne",
+		room: "G-01",
+		checkIn: "12-01-2023",
+		checkOut: "20-02-2023",
+		request:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis a condimentum vitae sapien pellentesque. Mauris pharetra et ultrices neque ornare aenean euismod elementum. ",
+		status: "Booked",
+	},
+	{
+		name: "Hal Jordan",
+		room: "G-01",
+		checkIn: "12-01-2023",
+		checkOut: "20-02-2023",
+		request:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis a condimentum vitae sapien pellentesque. Mauris pharetra et ultrices neque ornare aenean euismod elementum. ",
+		status: "Booked",
+	},
+	{
+		name: "Anthony Stark",
+		room: "G-01",
+		checkIn: "12-01-2023",
+		checkOut: "20-02-2023",
+		request:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis a condimentum vitae sapien pellentesque. Mauris pharetra et ultrices neque ornare aenean euismod elementum. ",
+		status: "Booked",
+	},
+	{
+		name: "Steve Rogers",
+		room: "G-01",
+		checkIn: "12-01-2023",
+		checkOut: "20-02-2023",
+		request:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis a condimentum vitae sapien pellentesque. Mauris pharetra et ultrices neque ornare aenean euismod elementum. ",
+		status: "Booked",
+	},
+	{
+		name: "Bruce Banner",
+		room: "G-01",
+		checkIn: "12-01-2023",
+		checkOut: "20-02-2023",
+		request:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis a condimentum vitae sapien pellentesque. Mauris pharetra et ultrices neque ornare aenean euismod elementum. ",
+		status: "Booked",
+	},
+	{
+		name: "Clint Barton",
+		room: "G-01",
+		checkIn: "12-01-2023",
+		checkOut: "20-02-2023",
+		request:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis a condimentum vitae sapien pellentesque. Mauris pharetra et ultrices neque ornare aenean euismod elementum. ",
+		status: "Booked",
+	},
+	{
+		name: "Nick Fury",
+		room: "G-01",
+		checkIn: "12-01-2023",
+		checkOut: "20-02-2023",
+		request:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis a condimentum vitae sapien pellentesque. Mauris pharetra et ultrices neque ornare aenean euismod elementum. ",
+		status: "Booked",
+	},
+	{
+		name: "Peter Parker",
+		room: "G-01",
+		checkIn: "12-01-2023",
+		checkOut: "20-02-2023",
+		request:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis a condimentum vitae sapien pellentesque. Mauris pharetra et ultrices neque ornare aenean euismod elementum. ",
+		status: "Booked",
+	},
+];
---
+++ src/services/pieData.ts
@@ -0,0 +1,32 @@
+export const pieData = [
+	{
+		id: "check in",
+		label: "check in",
+		value: 505,
+		color: "hsl(270, 70%, 50%)",
+	},
+	{
+		id: "check in",
+		label: "check in",
+		value: 524,
+		color: "hsl(180, 70%, 50%)",
+	},
+	{
+		id: "check in",
+		label: "check in",
+		value: 152,
+		color: "hsl(16, 70%, 50%)",
+	},
+	{
+		id: "check out",
+		label: "check out",
+		value: 13,
+		color: "hsl(82, 70%, 50%)",
+	},
+	{
+		id: "check out",
+		label: "check out",
+		value: 22,
+		color: "hsl(105, 70%, 50%)",
+	},
+];
