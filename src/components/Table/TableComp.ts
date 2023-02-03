eb6e00aefe6f42839fb7f58ad5f4fc77265f66fa
Sena543 <senanuazanu@gmail.com>
2023-02-03 12:55:34 +0000

    creation of generic table in progess

---
+++ src/components/Table/TableComp.ts
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+import { TableCell, tableCellClasses, TableRow } from "@mui/material";
+
+export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+	[`&.${tableCellClasses.head}`]: {
+		backgroundColor: "#f1faff",
+		// backgroundColor: theme.palette.common.black,
+		color: "#000",
+		fontWeight: "bold",
+		fontSize: 16,
+	},
+	[`&.${tableCellClasses.body}`]: {
+		fontSize: 14,
+	},
+}));
+
+export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+	"&:nth-of-type(odd)": {
+		// backgroundColor: theme.palette.action.hover,
+		backgroundColor: "#ebecf0",
+	},
+	// hide last border
+	"&:last-child td, &:last-child th": {
+		border: 0,
+	},
+}));
