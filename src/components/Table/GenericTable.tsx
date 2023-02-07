7c029656dfe090d54993df7f7b42c9763dfce015
Sena543 <senanuazanu@gmail.com>
2023-02-07 13:55:13 +0000

    request col

--- src/components/Table/GenericTable.tsx
+++ src/components/Table/GenericTable.tsx
@@ -15,13 +15,17 @@ import { styled } from "@mui/material/styles";
 import "./table.css";
 import { guests } from "../../services/guests";
 import { MoreHorizRounded } from "@mui/icons-material";
+import Request from "../guest/Request";
 import { StyledTableCell, StyledTableRow } from "./TableComp";
+import { useState } from "react";
 
 type GenericTableProps = {
 	tableData: any;
+	showActionCol?: boolean;
 };
 
-function GenericTable({ tableData }: GenericTableProps) {
+function GenericTable({ tableData, showActionCol }: GenericTableProps) {
+	const [open, setOpen] = useState<boolean>(false);
 	const header = Object.keys(tableData[0]);
 	return (
 		<TableContainer component={Paper} elevation={0} className="room-table-container">
@@ -29,26 +33,26 @@ function GenericTable({ tableData }: GenericTableProps) {
 				<TableHead>
 					<TableRow>
 						{header && header.map((name) => <StyledTableCell>{name.toLocaleUpperCase()}</StyledTableCell>)}
+						{showActionCol ? <StyledTableCell>Actions</StyledTableCell> : null}
 					</TableRow>
 				</TableHead>
 				<TableBody>
 					{tableData &&
 						tableData.map((data: any, index: number) => (
 							<StyledTableRow key={`${data["name"]} ${index}`} hover>
-								{/* {header.map((name, _) => {
-									console.log("toLocaleLowerCase:", data?.["checkout"]);
-									// console.log("toLocaleLowerCase:", data?.["checkout".toLowerCase()]);
-									return (
-										<StyledTableCell key={`${data["name"]}-${index}`}>
-											{data[name.toLowerCase().replaceAll(/\s/g, "")]}
-										</StyledTableCell>
-									);
-								})} */}
 								{header.map((name, _) => (
 									<StyledTableCell key={`${data["name"]}-${index}`}>
 										{data[name.toLowerCase().replaceAll(/\s/g, "")]}
 									</StyledTableCell>
 								))}
+								{showActionCol ? (
+									<StyledTableCell>
+										<IconButton onClick={() => setOpen(true)}>
+											<MoreHorizRounded />
+										</IconButton>
+									</StyledTableCell>
+								) : null}
+								<Request request={data["request"]} open={open} setOpen={setOpen} />
 							</StyledTableRow>
 						))}
 				</TableBody>
