8202cae5121b1c0cd0c5911289c559d57db115f7
Sena543 <senanuazanu@gmail.com>
2023-02-13 08:41:54 +0000

    guest details layout done

--- src/pages/Guest/GuestDetails/GuestDetails.tsx
+++ src/pages/Guest/GuestDetails/GuestDetails.tsx
@@ -1,5 +1,32 @@
+import "./guest-details.css";
+import GenericDashCards from "../../../components/Cards/GenericDashCards";
+import { Typography } from "@mui/material";
+import GuestProfile from "./GuestProfile";
+
 function GuestDetails() {
-	return <div>GuestDetails</div>;
+	return (
+		<div className="guest-details-container">
+			<div className="current-booking-card">
+				<GenericDashCards>Current Booking</GenericDashCards>
+			</div>
+			<div className="guest-profile-card">
+				<GenericDashCards>
+					<GuestProfile />
+				</GenericDashCards>
+			</div>
+
+			<div className="booking-history-title grid-col-span-3">
+				<Typography>Booking History</Typography>
+				<div className="date-report-div">
+					<div>Date filter</div>
+					<div>Generate Report</div>
+				</div>
+			</div>
+			<div className="guest-booking-history-card grid-col-span-3">
+				<GenericDashCards>Booking History</GenericDashCards>
+			</div>
+		</div>
+	);
 }
 
 export default GuestDetails;
