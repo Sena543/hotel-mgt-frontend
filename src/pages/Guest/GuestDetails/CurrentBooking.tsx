71c79b6f659cebb9b7369751eeb7d927682da74b
Sena543 <senanuazanu@gmail.com>
2023-02-13 14:15:55 +0000

    current booking component almost done

--- src/pages/Guest/GuestDetails/CurrentBooking.tsx
+++ src/pages/Guest/GuestDetails/CurrentBooking.tsx
@@ -1,5 +1,27 @@
+import { Typography } from "@mui/material";
+import "./current-booking.css";
+import imgSvg from "../../../assets/react.svg";
+
 function CurrentBooking() {
-	return <div>CurrentBooking</div>;
+	return (
+		<div className="current-booking-container">
+			<div>
+				<img src={imgSvg} alt="room image sample" />
+				<img src={imgSvg} alt="room image sample" />
+				<img src={imgSvg} alt="room image sample" />
+				<img src={imgSvg} alt="room image sample" />
+			</div>
+			{/* <div> */}
+			<div className="facilities-div">
+				<Typography>Room Facilities</Typography>
+				<Typography fontSize={14} color="gray">
+					AC, Shower, Double Bed, Bath tub, TV, Wifi
+				</Typography>
+			</div>
+			{/* </div> */}
+			<div className="room-details"> </div>
+		</div>
+	);
 }
 
 export default CurrentBooking;
