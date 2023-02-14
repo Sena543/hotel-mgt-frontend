6cde3d108e038e29d0bbbadc04afae379be00529
Sena543 <senanuazanu@gmail.com>
2023-02-14 16:37:27 +0000

    minor changes to styles

--- src/components/guest/Request.tsx
+++ src/components/guest/Request.tsx
@@ -9,56 +9,56 @@ const style = {
 	top: "50%",
 	left: "50%",
 	transform: "translate(-50%, -50%)",
-	width: 400,
+	width: 500,
 	bgcolor: "background.paper",
-	border: "2px solid #000",
+	border: "1px solid #000",
 	boxShadow: 24,
-	p: 4,
+	// p: 4,
+	padding: "10px",
 };
 
 function GuestRequest({ request, open, setOpen }: GuestRequestProps) {
 	const handleClose = () => setOpen(false);
 	return (
-		<div
-			style={{
-				borderRadius: 25,
-			}}
+		// <div
+		// 	style={{
+		// 		borderRadius: 25,
+		// 	}}
+		// >
+		<Modal
+			aria-labelledby="transition-modal-title"
+			aria-describedby="transition-modal-description"
+			open={open}
+			onClose={handleClose}
+			closeAfterTransition
+			// BackdropComponent={Backdrop}
+			// BackdropProps={{
+			// 	timeout: 500,
+			// }}
 		>
-			<Modal
-				aria-labelledby="transition-modal-title"
-				aria-describedby="transition-modal-description"
-				open={open}
-				onClose={handleClose}
-				closeAfterTransition
-				BackdropComponent={Backdrop}
-				BackdropProps={{
-					timeout: 500,
-				}}
-			>
-				<Fade in={open}>
-					<Box sx={style}>
-						<div
-							style={{
-								display: "flex",
-								flexDirection: "row",
-								// background: "red",
-								justifyContent: "space-between",
-								alignItems: "center",
-							}}
-						>
-							<Typography variant="h5">Guest Special Requests</Typography>
-							<IconButton onClick={handleClose}>
-								<CloseRounded />
-							</IconButton>
-						</div>
-						<Divider />
-						<Typography id="transition-modal-title" variant="h6">
-							{request}
-						</Typography>
-					</Box>
-				</Fade>
-			</Modal>
-		</div>
+			<Fade in={open}>
+				<Box sx={style}>
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							justifyContent: "space-between",
+							alignItems: "center",
+						}}
+					>
+						<Typography variant="h5">Guest Special Requests</Typography>
+						<IconButton onClick={handleClose}>
+							<CloseRounded />
+						</IconButton>
+					</div>
+					<Divider />
+					<Typography id="transition-modal-title" variant="h6">
+						{request}
+					</Typography>
+				</Box>
+			</Fade>
+		</Modal>
+		// </div>
 	);
 }
 
