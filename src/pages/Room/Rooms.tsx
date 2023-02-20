74129f2217f21f93b1d6a8aaaf8c40aa8fdb2c59
Sena543 <senanuazanu@gmail.com>
2023-02-20 13:04:59 +0000

    filter

--- src/pages/Room/Rooms.tsx
+++ src/pages/Room/Rooms.tsx
@@ -1,12 +1,15 @@
 import { Container } from "@mui/material";
+import { useState } from "react";
 import RoomHeader from "../../components/Room/RoomHeader";
 import RoomList from "../../components/Room/RoomList";
 
 function Rooms() {
+	const [selectedHeader, setSelectedHeader] = useState<string>("all");
+
 	return (
 		<Container className="room-container">
-			<RoomHeader />
-			<RoomList />
+			<RoomHeader selectedHeader={selectedHeader} setSelectedHeader={setSelectedHeader} />
+			<RoomList selectedHeader={selectedHeader} />
 		</Container>
 	);
 }
