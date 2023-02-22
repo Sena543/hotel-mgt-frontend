b8c9ad28ae541a7c41d0013e00463d24a238f4e2
Sena543 <senanuazanu@gmail.com>
2023-02-22 15:42:10 +0000

    theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./themes/theme";
 
 function App() {
 	return (
-		<div className="App">
-			<RouterProvider router={routes} />
-		</div>
+		<ThemeProvider theme={theme}>
+			<div className="App">
+				<RouterProvider router={routes} />
+			</div>
+		</ThemeProvider>
 	);
 }
 
