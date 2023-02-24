d8bb5080f1266605729a78433c7e59102110ac63
Sena543 <senanuazanu@gmail.com>
2023-02-24 08:29:48 +0000

    dynamic classnames passed through props

--- src/components/Cards/GenericDashCards.tsx
+++ src/components/Cards/GenericDashCards.tsx
@@ -1,10 +1,11 @@
 import "./card.css";
 type GenericDashCardsProps = {
 	children: any;
+	className?: string;
 };
 
-function GenericDashCards({ children }: GenericDashCardsProps) {
-	return <div className="generic-card-container">{children}</div>;
+function GenericDashCards({ children, className }: GenericDashCardsProps) {
+	return <div className={`generic-card-container ${className}`}>{children}</div>;
 }
 
 export default GenericDashCards;
