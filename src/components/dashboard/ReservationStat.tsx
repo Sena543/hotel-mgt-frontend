d0a25e4d893b0334858957f54e4c7e2e503ac39d
Sena543 <senanuazanu@gmail.com>
2023-02-24 09:10:14 +0000

    reservation stat done

--- src/components/dashboard/ReservationStat.tsx
+++ src/components/dashboard/ReservationStat.tsx
@@ -1,29 +1,51 @@
 import GenericDashCards from "../Cards/GenericDashCards";
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
 import { lineGraphData } from "../../services/lineGraphData";
-import { Typography } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
 import "./styles/reservation-stat.css";
+
+const PURPLE_LINE_GRAPH_COLOR = "#80529d";
+const YELLOW_LINE_GRAPH_COLOR = "#f6ae44";
 function ReservationStat() {
+	const theme = useTheme();
 	return (
 		<GenericDashCards>
 			<div className="reservation-stat-container">
-				<Typography fontWeight={"light"}>Reservation Statistics</Typography>
-				<div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "40%" }}>
+				<div className="reserve-stat-div">
+					<Typography fontWeight={"bold"}>Reservation Statistics</Typography>
+				</div>{" "}
+				<div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "50%" }}>
 					<div
-						style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "20%" }}
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							justifyContent: "space-between",
+							alignItems: "center",
+						}}
 					>
-						<Typography>123</Typography>
-						<Typography>Check in</Typography>
+						<Typography fontWeight="bold" fontSize={20} style={{ marginRight: "5px" }}>
+							123
+						</Typography>
+						<Typography style={{ color: "gray", fontSize: "13px" }}>Check in</Typography>
 					</div>
-					<div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-						<Typography>22</Typography>
-						<Typography>Check out</Typography>
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							justifyContent: "space-between",
+							alignItems: "center",
+						}}
+					>
+						<Typography fontWeight="bold" fontSize={20} style={{ marginRight: "5px" }}>
+							22
+						</Typography>
+						<Typography style={{ color: "gray", fontSize: "13px" }}>Check out</Typography>
 					</div>
 				</div>
 			</div>
 			<div style={{ width: "100%", height: "96%", display: "flex" }}>
 				<AreaChart
-					width={940}
+					width={930}
 					height={400}
 					data={lineGraphData}
 					margin={{
@@ -35,20 +57,31 @@ function ReservationStat() {
 				>
 					<defs>
 						<linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-							<stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-							<stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+							<stop offset="5%" stopColor={PURPLE_LINE_GRAPH_COLOR} stopOpacity={0.6} />
+							<stop offset="95%" stopColor={PURPLE_LINE_GRAPH_COLOR} stopOpacity={0} />
 						</linearGradient>
 						<linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-							<stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-							<stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+							<stop offset="5%" stopColor={YELLOW_LINE_GRAPH_COLOR} stopOpacity={0.6} />
+							<stop offset="95%" stopColor={YELLOW_LINE_GRAPH_COLOR} stopOpacity={0} />
 						</linearGradient>
 					</defs>
 					<XAxis dataKey="name" />
 					<YAxis />
-					{/* <CartesianGrid strokeDasharray="3 3" /> */}
 					<Tooltip />
-					<Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-					<Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+					<Area
+						type="monotone"
+						dataKey="checkIn"
+						stroke={PURPLE_LINE_GRAPH_COLOR}
+						fillOpacity={1}
+						fill="url(#colorUv)"
+					/>
+					<Area
+						type="monotone"
+						dataKey="checkOut"
+						stroke={YELLOW_LINE_GRAPH_COLOR}
+						fillOpacity={1}
+						fill="url(#colorPv)"
+					/>
 				</AreaChart>
 			</div>
 		</GenericDashCards>
