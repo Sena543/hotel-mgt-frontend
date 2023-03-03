d9b18be1db6bb7e9d9cd8993a6a2f5f9715be0ba
Sena543 <senanuazanu@gmail.com>
2023-03-03 12:24:10 +0000

    working on adding new guests manually

--- src/pages/Guest/Guests.tsx
+++ src/pages/Guest/Guests.tsx
@@ -1,17 +1,26 @@
-import { Typography } from "@mui/material";
+import { Typography, Button, useTheme } from "@mui/material";
+import { useState } from "react";
+import CreateGuestModal from "../../components/guest/CreateModal";
 import GuestTable from "../../components/guest/GuestTable";
 import "./guests.css";
 
 function Guests() {
+	const theme = useTheme();
+	const [openModal, setOpenModal] = useState(false);
 	return (
 		<div className="guest-container">
 			<div className="guest-title">
 				<Typography style={{}} fontSize={30} fontWeight="bold">
 					Guest List
 				</Typography>
+
+				<Button onClick={() => setOpenModal(true)} style={{ color: theme?.palette.primary.main }}>
+					Create Guest
+				</Button>
 			</div>
 
 			<GuestTable />
+			<CreateGuestModal open={openModal} setOpenModal={setOpenModal} />
 		</div>
 	);
 }
