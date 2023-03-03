b87917535abd5caf81964f730476707091344231
Sena543 <senanuazanu@gmail.com>
2023-03-03 12:23:46 +0000

    changed custom text style

--- src/components/TextInput/CustomTextField.tsx
+++ src/components/TextInput/CustomTextField.tsx
@@ -11,7 +11,7 @@ const CustomTextField = styled(TextField)({
 		"& fieldset": {
 			borderRadius: 10,
 			borderWidth: 0,
-			height: 40,
+			height: "100%",
 		},
 		// "&:hover fieldset": {
 		// 	boxShadow: "-5px -5px 21px 0.5px #A6EEF4",
