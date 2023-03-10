22691153125894677617df1c3e309e5231a55278
Sena543 <senanuazanu@gmail.com>
2023-03-10 10:49:46 +0000

    generic header. might improve later

---
+++ src/components/Header/GenericHeader.tsx
@@ -0,0 +1,37 @@
+import { Typography, Paper, Button } from "@mui/material";
+import React from "react";
+
+type GenericHeaderProps = {
+	headerTitle: string;
+};
+
+function GenericHeader({ headerTitle }: GenericHeaderProps) {
+	return (
+		<div className="staff-list-div">
+			<div>
+				<Typography style={{}} fontSize={30} fontWeight="bold">
+					{headerTitle}
+				</Typography>
+			</div>
+			{/*
+			<Paper className="room-header-paper">
+                 {headers &&
+					headers.map(({ name, value }) => (
+						<div
+							onClick={() => setSelectedHeader(value)}
+							key={`${name}-${value}`}
+							className={`roomHeader ${selectedHeader === value ? "header-selected" : null}`}
+						>
+							<Typography variant="h6">{name}</Typography>
+						</div>
+					))}
+			</Paper>
+                     */}
+			{/* <Button onClick={() => setOpenModal(true)} variant="contained">
+				Create Staff
+			</Button> */}
+		</div>
+	);
+}
+
+export default GenericHeader;
