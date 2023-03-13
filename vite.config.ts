5f4cc9f37eef12e13fb8880e82555dc46ff38381
Sena543 <senanuazanu@gmail.com>
2023-03-13 09:42:58 +0000

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/hotel-mgt-frontend/",
+	base: "/hotel-mgt-frontend",
 	plugins: [react()],
 });
