f2322c3565913d81e2ccddd9d8aad354993012f6
Sena543 <senanuazanu@gmail.com>
2023-03-14 14:43:58 +0000

    changed concierge to restaurant

---
+++ src/pages/Restuarant/Restuarant.tsx
@@ -0,0 +1,7 @@
+function Restuarant() {
+	// const headers = ["All Employees", "Active Employees", "Inactive Employees"];
+	// const tableheader = ["Employees Name", "Job Description", "Schedule", "Contact", "Status"];
+	return <div>Restuarant</div>;
+}
+
+export default Restuarant;
