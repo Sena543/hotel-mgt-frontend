1951257c67b57996066c3b018a1d4f0904a55467
Sena543 <senanuazanu@gmail.com>
2023-03-15 15:26:47 +0000

    restuarant table data

--- src/pages/Restuarant/Restuarant.tsx
+++ src/pages/Restuarant/Restuarant.tsx
@@ -1,7 +1,26 @@
+import { Button, Typography } from "@mui/material";
+import GenericTable from "../../components/Table/GenericTable";
+
 function Restuarant() {
-	// const headers = ["All Employees", "Active Employees", "Inactive Employees"];
-	// const tableheader = ["Employees Name", "Job Description", "Schedule", "Contact", "Status"];
-	return <div>Restuarant</div>;
+	const restaurantList = [
+		{ guestid: 1, guestname: "Bruce Wayne", dishordered: "Rice", price: 100 },
+		{ guestid: 2, guestname: "Bruce Wayne", dishordered: "Rice", price: 100 },
+		{ guestid: 3, guestname: "Bruce Wayne", dishordered: "Rice", price: 100 },
+		{ guestid: 4, guestname: "Bruce Wayne", dishordered: "Rice", price: 100 },
+	];
+
+	console.log(restaurantList);
+	return (
+		<div>
+			<div>
+				<Typography>Restuarant</Typography>
+				<Button></Button>
+			</div>
+			<div className="guest-restuarant-orders">
+				<GenericTable tableData={restaurantList} />
+			</div>
+		</div>
+	);
 }
 
 export default Restuarant;
