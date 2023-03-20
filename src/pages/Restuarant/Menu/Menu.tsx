61ea717280bfea2413cb3957cd2ce486699e0863
Sena543 <senanuazanu@gmail.com>
2023-03-20 15:56:37 +0000

    menu list

--- src/pages/Restuarant/Menu/Menu.tsx
+++ src/pages/Restuarant/Menu/Menu.tsx
@@ -1,34 +1,128 @@
 import "./menu.css";
 import { Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import MenuModal from "../../../components/Restaurant/MenuModal";
+import { useState } from "react";
 
+//www.benitos.com/menu/
 function Menu() {
-	return (
-		<div className="menu-container">
-			<div className="menu-header">
-				<Typography style={{}} fontSize={30} fontWeight="bold">
-					Menu
-				</Typography>
+	const [open, setModalOpen] = useState(false);
+	const meals = [
+		{
+			meal: "Chicken Taco",
+			price: "3.99",
+			description:
+				"Succulent hand-shredded pork, grilled then smothered with our fresh homemade guacamole and salsa Mexicana, wrapped in a warm, soft corn tortilla.",
+			mealType: "breakfast",
+		},
+		{
+			meal: "Chicken Taco",
+			price: "3.99",
+			description:
+				"Succulent hand-shredded pork, grilled then smothered with our fresh homemade guacamole and salsa Mexicana, wrapped in a warm, soft corn tortilla.",
+			mealType: "lunch",
+		},
+		{
+			meal: "Beef Taco",
+			price: "3.99",
+			description:
+				"Succulent hand-shredded pork, grilled then smothered with our fresh homemade guacamole and salsa Mexicana, wrapped in a warm, soft corn tortilla.",
+			mealType: "supper",
+		},
+		{
+			meal: "CARNE ASADA Taco",
+			price: "3.99",
+			description:
+				"Succulent hand-shredded pork, grilled then smothered with our fresh homemade guacamole and salsa Mexicana, wrapped in a warm, soft corn tortilla.",
+			mealType: "breakfast",
+		},
+		{
+			meal: "Chicken Taco",
+			price: "3.99",
+			description:
+				"Succulent hand-shredded pork, grilled then smothered with our fresh homemade guacamole and salsa Mexicana, wrapped in a warm, soft corn tortilla.",
+			mealType: "lunch",
+		},
+		{
+			meal: "Chicken Taco",
+			price: "3.99",
+			description:
+				"Succulent hand-shredded pork, grilled then smothered with our fresh homemade guacamole and salsa Mexicana, wrapped in a warm, soft corn tortilla.",
+			mealType: "supper",
+		},
+		{
+			meal: "Chicken Taco",
+			price: "3.99",
+			description:
+				"Succulent hand-shredded pork, grilled then smothered with our fresh homemade guacamole and salsa Mexicana, wrapped in a warm, soft corn tortilla.",
+			mealType: "breakfast",
+		},
+		{
+			meal: "Chicken Taco",
+			price: "3.99",
+			description:
+				"Succulent hand-shredded pork, grilled then smothered with our fresh homemade guacamole and salsa Mexicana, wrapped in a warm, soft corn tortilla.",
+			mealType: "lunch",
+		},
+		{
+			meal: "Chicken Taco",
+			price: "3.99",
+			description:
+				"Succulent hand-shredded pork, grilled then smothered with our fresh homemade guacamole and salsa Mexicana, wrapped in a warm, soft corn tortilla.",
+			mealType: "supper",
+		},
+	];
 
-				<div className="menu-button-div">
-					<Link className="menu-link" to={"/restaurant"}>
-						<Button>Back</Button>
-					</Link>
-				</div>
-			</div>
-			<div>
-				<div className="food-header">
-					<h2>Breakfast</h2>
-				</div>
-				<div className="food-list">
-					<div className="dish">
-						<Typography>CHICKEN TACO</Typography>
-						<span className="element-sep"></span>
-						<Typography>3.69</Typography>
+	const breakfast = meals.filter(({ mealType }) => mealType === "breakfast");
+	const lunch = meals.filter(({ mealType }) => mealType === "lunch");
+	const dinner = meals.filter(({ mealType }) => mealType === "supper");
+
+	const renderMealsAndPrices = (mealFilter: any) => {
+		return (
+			<>
+				{mealFilter &&
+					mealFilter.map(({ meal, price }: { meal: string; price: number }) => (
+						<>
+							<div className="dish">
+								<Typography variant="h5">{meal.toLocaleUpperCase()}</Typography>
+								<span className="element-sep"></span>
+								<Typography variant="h4">{price}</Typography>
+							</div>
+						</>
+					))}
+			</>
+		);
+	};
+
+	return (
+		<>
+			<div className="menu-container">
+				<div className="menu-header">
+					<Typography style={{}} fontSize={30} fontWeight="bold">
+						Menu
+					</Typography>
+
+					<div className="menu-button-div">
+						<Button onClick={() => setModalOpen(true)}>Add New Dish</Button>
 					</div>
 				</div>
+				<div>
+					<div className="food-header">
+						<h2>Breakfast</h2>
+					</div>
+					<div className="food-list">{renderMealsAndPrices(breakfast)}</div>
+					<div className="food-header">
+						<h2>Lunch</h2>
+					</div>
+					<div className="food-list">{renderMealsAndPrices(lunch)}</div>
+					<div className="food-header">
+						<h2>Dinner</h2>
+					</div>
+					<div className="food-list">{renderMealsAndPrices(dinner)}</div>
+				</div>
 			</div>
-		</div>
+			<MenuModal open={open} setOpenModal={setModalOpen} />
+		</>
 	);
 }
 
