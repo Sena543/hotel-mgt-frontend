6fa84445ddf8bbac7796bddb805022dcf4db71db
Sena543 <senanuazanu@gmail.com>
2023-03-20 13:29:09 +0000

    ...

--- src/pages/Restuarant/Menu/Menu.tsx
+++ src/pages/Restuarant/Menu/Menu.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import "./menu.css";
+import { Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Menu() {
-	return <div>Menu</div>;
+	return (
+		<div className="menu-container">
+			<div className="menu-header">
+				<Typography style={{}} fontSize={30} fontWeight="bold">
+					Menu
+				</Typography>
+
+				<div className="menu-button-div">
+					<Link className="menu-link" to={"/restaurant"}>
+						<Button>Back</Button>
+					</Link>
+				</div>
+			</div>
+			<div>
+				<div className="food-header">
+					<h2>Breakfast</h2>
+				</div>
+				<div className="food-list">
+					<div className="dish">
+						<Typography>CHICKEN TACO</Typography>
+						<span className="element-sep"></span>
+						<Typography>3.69</Typography>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Menu;
