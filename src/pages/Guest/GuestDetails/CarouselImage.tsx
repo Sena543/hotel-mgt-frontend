ec1b4aaa902d9c17b8c815acda64db77ec0d93fe
Sena543 <senanuazanu@gmail.com>
2023-03-23 14:35:30 +0000

    ....

--- src/pages/Guest/GuestDetails/CarouselImage.tsx
+++ src/pages/Guest/GuestDetails/CarouselImage.tsx
@@ -9,9 +9,10 @@ function CarouselImage({ imgUrl }: { imgUrl: string }) {
 				alignItems: "center",
 				height: "300px",
 				width: "100%",
+				margin: "10px",
 			}}
 		>
-			<img loading="lazy" src={imgUrl} alt="room image sample" />
+			<img style={{ borderRadius: "10px" }} loading="lazy" src={imgUrl} alt="room sample" />
 		</div>
 	);
 }
