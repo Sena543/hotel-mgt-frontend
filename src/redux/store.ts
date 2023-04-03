f8fb22fa49a1e9b55f1434d281ed25863d638fbe
Sena543 <senanuazanu@gmail.com>
2023-04-03 13:21:10 +0000

    room reducsers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import guestsReducer from "./slices/guestSlices";
+import roomReducer from "./slices/roomSlicers";
 
 export default configureStore({
 	reducer: {
 		guests: guestsReducer,
+		rooms: roomReducer,
 	},
 });
