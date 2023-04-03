82c203c0138c72a47f4b2b175b5ed1054dfd9b77
Sena543 <senanuazanu@gmail.com>
2023-04-03 11:48:08 +0000

    add new guest with dispatch

--- src/components/guest/CreateGuestModal.tsx
+++ src/components/guest/CreateGuestModal.tsx
@@ -1,6 +1,15 @@
 import "./create-guest.css";
-import { CloseRounded } from "@mui/icons-material";
-import { Button, Typography, IconButton, Divider, Tooltip, TextField, Autocomplete } from "@mui/material";
+import { CleanHands, CloseRounded } from "@mui/icons-material";
+import {
+	Button,
+	Typography,
+	IconButton,
+	Divider,
+	Tooltip,
+	TextField,
+	Autocomplete,
+	CircularProgress,
+} from "@mui/material";
 import GenericModal from "../Modal/GenericModal";
 import CustomTextField from "../TextInput/CustomTextField";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
@@ -12,6 +21,9 @@ import customParseFormat from "dayjs/plugin/customParseFormat";
 import { useMemo, useState } from "react";
 import { addDoc, collection } from "firebase/firestore/lite";
 import firestoredb from "../../../firebase-config";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewGuest } from "../../redux/slices/guestSlices";
+import { AppDispatch } from "../../redux/types";
 
 type CreateModalProps = {
 	open: boolean;
@@ -28,9 +40,12 @@ function CreateGuestModal({ setOpenModal, open }: CreateModalProps) {
 		checkIn: dayjs(), //today
 		checkOut: dayjs().add(1, "day"), //tomorrow
 		roomAssigned: "GR-R02",
+		specialRequests: "",
 	});
 	const [dateError, setDateError] = useState<DateValidationError | null>(null);
-
+	const dispatch = useDispatch<AppDispatch>();
+	const { status } = useSelector((state: any) => state.guests);
+	console.log(status);
 	const errorMessage = useMemo(() => {
 		switch (dateError) {
 			// case "maxDate":
@@ -71,8 +86,7 @@ function CreateGuestModal({ setOpenModal, open }: CreateModalProps) {
 	};
 
 	const submitGuestData = async () => {
-		const guestCollectionRef = collection(firestoredb, "guests");
-		await addDoc(guestCollectionRef, {
+		const newGuestData = {
 			...guestDetails,
 			checkIn: `${guestDetails["checkIn"].get("date")}-${guestDetails["checkIn"].get("month") + 1}-${guestDetails[
 				"checkIn"
@@ -80,8 +94,9 @@ function CreateGuestModal({ setOpenModal, open }: CreateModalProps) {
 			checkOut: `${guestDetails["checkOut"].get("date")}-${
 				guestDetails["checkOut"].get("month") + 1
 			}-${guestDetails["checkOut"].get("year")}`,
-		});
-		// console.log(addNewGuestData);
+		};
+		console.log(newGuestData);
+		dispatch(addNewGuest(newGuestData));
 	};
 
 	return (
@@ -206,14 +221,18 @@ function CreateGuestModal({ setOpenModal, open }: CreateModalProps) {
 						</div>
 					</div>
 					<div className="button-div">
-						<Button
-							// onClick={() => console.log(guestDetails)}
-							onClick={submitGuestData}
-							variant="contained"
-							className="create-guest-button"
-						>
-							Create
-						</Button>
+						{status === "loading" ? (
+							<CircularProgress />
+						) : (
+							<Button
+								// onClick={() => console.log(guestDetails)}
+								onClick={submitGuestData}
+								variant="contained"
+								className="create-guest-button"
+							>
+								Create
+							</Button>
+						)}
 					</div>
 				</div>
 			</GenericModal>
@@ -222,31 +241,5 @@ function CreateGuestModal({ setOpenModal, open }: CreateModalProps) {
 }
 
 export default CreateGuestModal;
-// <Modal
-// 	className="modal"
-// 	open={open}
-// 	onClose={() => setOpenModal(false)}
-// 	aria-labelledby="modal-modal-title"
-// 	aria-describedby="modal-modal-description"
-// >
-// 	<Box className="modal-box">
-// 		{/* <Box sx={style} className="modal-box"> */}
-// 		<div className="guest-review-header generic-flex-justify-content-style">
-// 			<Typography fontWeight={"bolder"} fontSize="25px">
-// 				New Guest
-// 			</Typography>
-// 			<Tooltip title="Close">
-// 				<IconButton onClick={() => setOpenModal(false)}>
-// 					<CloseRounded />
-// 				</IconButton>
-// 			</Tooltip>
-// 		</div>
-// 		<Divider />
-// 		<div className="create-guest-form">
-// 			<div>
-// 				<TextField name="Last Name" />
-// 				<TextField name="First Name" />
-// 			</div>
-// 		</div>
-// 	</Box>
-// </Modal>
+// TODO
+//show notification after add/error has occured
