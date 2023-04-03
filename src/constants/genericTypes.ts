317b79835832334be4239489e562b95913c8bfb6
Sena543 <senanuazanu@gmail.com>
2023-04-03 10:23:59 +0000

    generic types

---
+++ src/constants/genericTypes.ts
@@ -0,0 +1,11 @@
+export type GuestsType = {
+	lastName: string;
+	firstName: string;
+	roomAssigned: string;
+	checkIn: string | any;
+	checkOut: string | any;
+	specialRequests: string;
+	status: string;
+	email: string;
+	contact: string;
+};
