10af57fddf6f83fdbc17003471edc2911b4a77d2
Sena543 <senanuazanu@gmail.com>
2023-04-03 10:23:47 +0000

    store config

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./themes/theme";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
 	return (
-		<ThemeProvider theme={theme}>
-			<div className="App">
-				<RouterProvider router={routes} />
-			</div>
-		</ThemeProvider>
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<div className="App">
+					<RouterProvider router={routes} />
+				</div>
+			</ThemeProvider>
+		</Provider>
 	);
 }
 
