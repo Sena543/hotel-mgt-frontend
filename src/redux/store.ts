5ea3cc7b8b32925cce8ec958c2fc0f6934cbb429
Sena543 <senanuazanu@gmail.com>
2023-04-04 13:27:10 +0000

    staff reducer for store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import guestsReducer from "./slices/guestSlices";
 import roomReducer from "./slices/roomSlicers";
+import staffReducer from "./slices/staffSlices";
 
 export default configureStore({
 	reducer: {
 		guests: guestsReducer,
 		rooms: roomReducer,
+		staff: staffReducer,
 	},
 });
