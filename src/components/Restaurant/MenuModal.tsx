4f4b7940bdfc4c0cfcd3d5d70e9a24a215157c6c
Sena543 <senanuazanu@gmail.com>
2023-04-04 15:52:54 +0000

    menu modal done

--- src/components/Restaurant/MenuModal.tsx
+++ src/components/Restaurant/MenuModal.tsx
@@ -1,17 +1,53 @@
 import "./menu-modal.css";
 import { CloseRounded } from "@mui/icons-material";
-import { Typography, IconButton, Divider, Autocomplete, Button, Tooltip } from "@mui/material";
+import {
+	Typography,
+	IconButton,
+	Divider,
+	Autocomplete,
+	Button,
+	Tooltip,
+	FormControl,
+	FormControlLabel,
+	FormLabel,
+	Radio,
+	RadioGroup,
+} from "@mui/material";
 import GenericModal from "../Modal/GenericModal";
 import CustomTextField from "../TextInput/CustomTextField";
+import { useState } from "react";
 
 type MenuModalProps = {
 	open: boolean;
 	setOpenModal: Function;
 };
 
-const dish_type = [{ label: "Breakfast" }, { label: "Lunch" }, { label: "Dinner" }];
+const dish_type = ["Breakfast", "Lunch", "Dinner"];
 
 function MenuModal({ open, setOpenModal }: MenuModalProps) {
+	const [menuItem, setMenuItem] = useState({
+		menuType: "dish",
+		dishOrBev: "",
+		price: "",
+		dishType: "Breakfast",
+		description: "",
+	});
+
+	const handleRadioChange = (name: string, value: string) => {
+		setMenuItem({ ...menuItem, [name]: value });
+	};
+
+	const submitData = () => {
+		console.log(menuItem);
+
+		// setMenuItem({
+		// 	menuType: "dish",
+		// 	dishOrBev: "",
+		// 	price: "",
+		// 	dishType: "Breakfast",
+		// 	description: "",
+		// });
+	};
 	return (
 		<GenericModal className="menu-modal-container" open={open} setOpenModal={setOpenModal}>
 			<div className="menu-modal-header">
@@ -24,30 +60,121 @@ function MenuModal({ open, setOpenModal }: MenuModalProps) {
 			</div>
 			<Divider />
 			<div className="dish-form">
-				<CustomTextField label="Meal" className="menu-modal-text-field" />
-				<CustomTextField label="Price" className="menu-modal-text-field" />
+				<div>
+					<FormControl>
+						<RadioGroup
+							value={menuItem.menuType}
+							onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+								handleRadioChange(event.target.name, event.target.value);
+							}}
+							row
+							aria-labelledby="demo-row-radio-buttons-group-label"
+							name="menuType"
+							defaultValue={"dish"}
+						>
+							<FormControlLabel value="dish" control={<Radio />} label="Dishes" />
+							<FormControlLabel value="beverage" control={<Radio />} label="Beverages" />
+						</RadioGroup>
+					</FormControl>
+				</div>
+				{menuItem.menuType === "dish" ? (
+					<>
+						<CustomTextField
+							label="Meal"
+							name="dishOrBev"
+							onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+								handleRadioChange(event.target.name, event.target.value)
+							}
+							value={menuItem.dishOrBev}
+							className="menu-modal-text-field"
+						/>
+						<CustomTextField
+							label="Price"
+							onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+								handleRadioChange(event.target.name, event.target.value)
+							}
+							name="price"
+							value={menuItem.price}
+							className="menu-modal-text-field"
+						/>
 
-				<div className="menu-autocomplete">
-					<Autocomplete
-						disablePortal
-						id="combo-box-demo"
-						options={dish_type}
-						fullWidth
-						className="menu-modal-text-field"
-						renderInput={(params) => (
-							<CustomTextField
-								{...params}
+						<div className="menu-autocomplete">
+							<Autocomplete
+								disablePortal
+								id="combo-box-demo"
+								options={dish_type}
+								isOptionEqualToValue={(option, value) => option === value}
+								getOptionLabel={(option: string) => option}
+								fullWidth
+								value={menuItem.dishType}
+								onChange={(event: React.SyntheticEvent<Element, Event>, value: any) =>
+									handleRadioChange("dishType", value)
+								}
+								className="menu-modal-text-field"
+								renderInput={(params) => (
+									<CustomTextField
+										{...params}
+										fullWidth
+										className="menu-modal-text-field"
+										label="Dish Type"
+									/>
+								)}
+							/>
+						</div>
+						<CustomTextField className="menu-modal-text-field" label="Description" rows={5} multiline />
+					</>
+				) : (
+					<>
+						<CustomTextField
+							name="dishOrBev"
+							onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+								handleRadioChange(event.target.name, event.target.value)
+							}
+							label="Beverage"
+							className="menu-modal-text-field"
+						/>
+						<CustomTextField
+							name="price"
+							onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+								handleRadioChange(event.target.name, event.target.value)
+							}
+							label="Price"
+							className="menu-modal-text-field"
+						/>
+
+						{/* <div className="menu-autocomplete">
+							<Autocomplete
+								disablePortal
+								id="combo-box-demo"
+								options={dish_type}
 								fullWidth
 								className="menu-modal-text-field"
-								label="Dish Type"
+								renderInput={(params) => (
+									<CustomTextField
+										{...params}
+										fullWidth
+										className="menu-modal-text-field"
+										label="Dish Type"
+									/>
+								)}
 							/>
-						)}
-					/>
-				</div>
-				<CustomTextField className="menu-modal-text-field" label="Description" rows={5} multiline />
+						</div> */}
+						<CustomTextField
+							name="description"
+							value={menuItem.description}
+							onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+								handleRadioChange(event.target.name, event.target.value)
+							}
+							className="menu-modal-text-field"
+							label="Description"
+							rows={5}
+							multiline
+						/>
+					</>
+				)}
 
 				<div className="button-div">
-					<Button variant="contained" className="create-order-button">
+					<Button onClick={submitData} variant="contained" className="create-order-button">
 						Add
 					</Button>
 				</div>
