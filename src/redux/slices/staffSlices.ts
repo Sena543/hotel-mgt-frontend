1e5f15e9cace672f8f68553b3ace1ecfe0cfc5f3
Sena543 <senanuazanu@gmail.com>
2023-04-04 13:24:50 +0000

    staffSlice almost done

---
+++ src/redux/slices/staffSlices.ts
@@ -0,0 +1,80 @@
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getDocs, collection, addDoc } from "firebase/firestore/lite";
+import firestoredb from "../../../firebase-config";
+import { getRawData } from "../../utils/util-functions";
+import { StaffDetailsType } from "../../constants/genericTypes";
+
+const initialState = {
+	status: "idle", // 'idle' | 'loading' | 'success'| 'failed
+	staffData: [],
+	errorMessage: "",
+};
+
+export const fetchAllStaff = createAsyncThunk("fetch/staff", async function fetchStaff(_, thunkAPI) {
+	try {
+		const returnedGuestsData = await getDocs(collection(firestoredb, "employees"));
+		const staffData: any = getRawData(returnedGuestsData);
+
+		return staffData;
+	} catch (error: any) {
+		console.log(error.message);
+
+		return thunkAPI.rejectWithValue(error.message);
+	}
+});
+
+export const createNewStaff = createAsyncThunk(
+	"create/employees",
+	async function fetchStaff(newEmployeeData: StaffDetailsType, thunkAPI) {
+		try {
+			await addDoc(collection(firestoredb, "employees"), newEmployeeData);
+			return newEmployeeData;
+		} catch (error: any) {
+			console.log(error);
+
+			return thunkAPI.rejectWithValue(error.message);
+		}
+	}
+);
+
+export const staffSlice = createSlice({
+	name: "staff",
+	initialState,
+	reducers: {
+		resetStatus: (state) => {
+			state = { ...state, status: "idle" };
+		},
+	},
+	extraReducers(builder) {
+		builder.addCase(fetchAllStaff.pending, (state) => {
+			state = { ...state, status: "loading" };
+			return state;
+		});
+		builder.addCase(fetchAllStaff.fulfilled, (state, action: PayloadAction<any>) => {
+			state = { ...state, status: "sucess", staffData: action.payload };
+			return state;
+		});
+
+		builder.addCase(fetchAllStaff.rejected, (state, action: PayloadAction<any>) => {
+			state = { ...state, status: "failed", errorMessage: action.payload };
+			return state;
+		});
+		builder.addCase(createNewStaff.pending, (state) => {
+			state = { ...state, status: "loading" };
+			return state;
+		});
+		builder.addCase(createNewStaff.fulfilled, (state: any, action: PayloadAction<StaffDetailsType>) => {
+			console.log(action.payload);
+			state = { ...state, staffData: [...state.staffData, action.payload], status: "success" };
+			return state;
+		});
+		builder.addCase(createNewStaff.rejected, (state, action: any) => {
+			state = { ...state, status: "failed", errorMessage: action.payload };
+			// state = { ...state, status: action.payload };
+			return state;
+		});
+	},
+});
+
+export const { resetStatus } = staffSlice.actions;
+export default staffSlice.reducer;
