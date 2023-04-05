20ce4fcff47e293484a01ebc2bb06dcaa8338389
Sena543 <senanuazanu@gmail.com>
2023-04-05 11:14:36 +0000

    restuarant slice

---
+++ src/redux/slices/restaurantSlice.ts
@@ -0,0 +1,84 @@
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getDocs, collection, addDoc } from "firebase/firestore/lite";
+import firestoredb from "../../../firebase-config";
+import { getRawData } from "../../utils/util-functions";
+import { MenuItemType } from "../../constants/genericTypes";
+
+const initialState = {
+	status: "idle", // 'idle' | 'loading' | 'success'| 'failed
+	restaurantMealsList: [],
+	errorMessage: "",
+};
+
+export const fetchRestaurantMenu = createAsyncThunk("fetch/restaurant-meals", async function fetchStaff(_, thunkAPI) {
+	try {
+		const returnedMenuData = await getDocs(collection(firestoredb, "restaurant"));
+		const menuData: any = getRawData(returnedMenuData);
+
+		return menuData;
+	} catch (error: any) {
+		console.log(error.message);
+
+		return thunkAPI.rejectWithValue(error.message);
+	}
+});
+
+export const createNewMenuItem = createAsyncThunk(
+	"create/new-menu-item",
+	async function fetchStaff(newMenuItemData: MenuItemType, thunkAPI) {
+		try {
+			await addDoc(collection(firestoredb, "restaurant"), newMenuItemData);
+			return newMenuItemData;
+		} catch (error: any) {
+			console.log(error);
+
+			return thunkAPI.rejectWithValue(error.message);
+		}
+	}
+);
+
+export const restaurantSlice = createSlice({
+	name: "booking",
+	initialState,
+	reducers: {
+		resetStatus: (state) => {
+			state = { ...state, status: "idle" };
+		},
+	},
+	extraReducers(builder) {
+		builder.addCase(fetchRestaurantMenu.pending, (state) => {
+			state = { ...state, status: "loading" };
+			return state;
+		});
+		builder.addCase(fetchRestaurantMenu.fulfilled, (state, action: PayloadAction<any>) => {
+			state = { ...state, status: "sucess", restaurantMealsList: action.payload };
+
+			return state;
+		});
+
+		builder.addCase(fetchRestaurantMenu.rejected, (state, action: PayloadAction<any>) => {
+			state = { ...state, status: "failed", errorMessage: action.payload };
+			return state;
+		});
+		builder.addCase(createNewMenuItem.pending, (state) => {
+			state = { ...state, status: "loading" };
+			return state;
+		});
+		builder.addCase(createNewMenuItem.fulfilled, (state: any, action: PayloadAction<MenuItemType>) => {
+			state = {
+				...state,
+				restaurantMealsList: [...state.restaurantMealsList, action.payload],
+				status: "success",
+			};
+			return state;
+		});
+		builder.addCase(createNewMenuItem.rejected, (state, action: any) => {
+			state = { ...state, status: "failed", errorMessage: action.payload };
+			// state = { ...state, status: action.payload };
+			return state;
+		});
+	},
+});
+
+export const { resetStatus } = restaurantSlice.actions;
+export default restaurantSlice.reducer;
