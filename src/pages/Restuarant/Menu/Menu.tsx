ce337d2348bc6633f40447079827ba3881bdd8e2
Sena543 <senanuazanu@gmail.com>
2023-04-05 15:20:39 +0000

    everything rendering properly

--- src/pages/Restuarant/Menu/Menu.tsx
+++ src/pages/Restuarant/Menu/Menu.tsx
@@ -4,18 +4,20 @@ import { Link } from "react-router-dom";
 import MenuModal from "../../../components/Restaurant/MenuModal";
 import { useEffect, useState } from "react";
 import { DeleteForeverRounded } from "@mui/icons-material";
-import { useDispatch } from "react-redux";
-import { fetchRestaurantMenu } from "../../../redux/slices/restaurantSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRestaurantMenu, deleteMenuItem } from "../../../redux/slices/restaurantSlice";
 import { AppDispatch } from "../../../redux/types";
+import { filterMenuItems } from "../../../utils/util-functions";
 
 // www.benitos.com/menu/
 function Menu() {
 	const dispatch = useDispatch<AppDispatch>();
+	const { restaurantMealsList } = useSelector((state: any) => state.restaurant);
 	const [open, setModalOpen] = useState(false);
 
 	useEffect(() => {
 		dispatch(fetchRestaurantMenu());
-	});
+	}, []);
 	const meals = [
 		{
 			meal: "Chicken Taco",
@@ -82,26 +84,57 @@ function Menu() {
 		},
 	];
 
-	const breakfast = meals.filter(({ mealType }) => mealType === "breakfast");
-	const lunch = meals.filter(({ mealType }) => mealType === "lunch");
-	const dinner = meals.filter(({ mealType }) => mealType === "supper");
+	const breakfast = filterMenuItems(restaurantMealsList, "breakfast", "dish");
+	const lunch = filterMenuItems(restaurantMealsList, "lunch", "dish");
+	const dinner = filterMenuItems(restaurantMealsList, "dinner", "dish");
+	const beverage = filterMenuItems(restaurantMealsList, "beverage", "beverage");
+	// const breakfast = meals.filter(({ mealType }) => mealType === "breakfast");
+	// const lunch = meals.filter(({ mealType }) => mealType === "lunch");
+	// const dinner = meals.filter(({ mealType }) => mealType === "supper");
 
+	// console.log("restaurant", restaurantMealsList);
+	// console.log("bevs", beverage);
+
+	const deleteItemFromMenu = (dishId: any) => {
+		// dispatch(deleteMenuItem(dishId));
+		//TODO
+		//delete menu item
+	};
 	const renderMealsAndPrices = (mealFilter: any) => {
 		return (
 			<>
 				{mealFilter &&
-					mealFilter.map(({ meal, price }: { meal: string; price: number }, index: number) => (
-						<div key={`${index}`} className={`dish slide-left`}>
-							<Typography variant="h5">{meal.toLocaleUpperCase()}</Typography>
-							<span className="element-sep"></span>
-							<Typography variant="h4">{price}</Typography>
-							<div className="delete-menu-item-icon">
-								<IconButton>
-									<DeleteForeverRounded />
-								</IconButton>
+					mealFilter.map(
+						(
+							{
+								price,
+								// dishType,
+								// dishId,
+								// description,
+								// menuType,
+								dishOrBev,
+							}: {
+								price: string;
+								dishType: string;
+								dishId: string;
+								description: string;
+								menuType: string;
+								dishOrBev: string;
+							},
+							index: number
+						) => (
+							<div key={`${index}`} className={`dish slide-left`}>
+								<Typography variant="h5">{dishOrBev.toLocaleUpperCase()}</Typography>
+								<span className="element-sep"></span>
+								<Typography variant="h4">{price}</Typography>
+								<div className="delete-menu-item-icon">
+									<IconButton onClick={() => deleteItemFromMenu(index)}>
+										<DeleteForeverRounded />
+									</IconButton>
+								</div>
 							</div>
-						</div>
-					))}
+						)
+					)}
 			</>
 		);
 	};
@@ -130,15 +163,15 @@ function Menu() {
 					<div className="food-header">
 						<h2>Dinner</h2>
 					</div>
-					<div className="food-list">{renderMealsAndPrices(dinner)}</div>
+					{/* <div className="food-list">{renderMealsAndPrices(dinner)}</div> */}
 					<div className="beverages-sides-container">
-						<div className="food-header sides-beverages-div">
+						{/* <div className="food-header sides-beverages-div">
 							<h2>Sides</h2>
 							<div className="food-list">{renderMealsAndPrices(dinner)}</div>
-						</div>
+						</div> */}
 						<div className="food-header sides-beverages-div">
 							<h2>Bevrages</h2>
-							<div className="food-list">{renderMealsAndPrices(dinner)}</div>
+							<div className="food-list">{renderMealsAndPrices(beverage)}</div>
 						</div>
 					</div>
 				</div>
