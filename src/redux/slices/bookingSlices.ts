8ff8443f6f0f0b3073df0d75c8572a6a52924d11
Sena543 <senanuazanu@gmail.com>
2023-04-05 09:47:58 +0000

    booking slice

---
+++ src/redux/slices/bookingSlices.ts
@@ -0,0 +1,83 @@
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getDocs, collection, addDoc } from "firebase/firestore/lite";
+import firestoredb from "../../../firebase-config";
+import { getRawData } from "../../utils/util-functions";
+import { StaffDetailsType } from "../../constants/genericTypes";
+
+const initialState = {
+	status: "idle", // 'idle' | 'loading' | 'success'| 'failed
+	bookingHistory: [],
+	errorMessage: "",
+};
+
+export const fetchGuestBookingHistory = createAsyncThunk(
+	"fetch/booking-history",
+	async function fetchStaff(_, thunkAPI) {
+		try {
+			const returnedGuestsData = await getDocs(collection(firestoredb, "booking"));
+			const staffData: any = getRawData(returnedGuestsData);
+
+			return staffData;
+		} catch (error: any) {
+			console.log(error.message);
+
+			return thunkAPI.rejectWithValue(error.message);
+		}
+	}
+);
+
+export const createNewBookingHistory = createAsyncThunk(
+	"create/booking-history",
+	async function fetchStaff(newBookingHistoryData: StaffDetailsType, thunkAPI) {
+		try {
+			await addDoc(collection(firestoredb, "booking"), newBookingHistoryData);
+			return newBookingHistoryData;
+		} catch (error: any) {
+			console.log(error);
+
+			return thunkAPI.rejectWithValue(error.message);
+		}
+	}
+);
+
+export const bookingSlice = createSlice({
+	name: "booking",
+	initialState,
+	reducers: {
+		resetStatus: (state) => {
+			state = { ...state, status: "idle" };
+		},
+	},
+	extraReducers(builder) {
+		builder.addCase(fetchGuestBookingHistory.pending, (state) => {
+			state = { ...state, status: "loading" };
+			return state;
+		});
+		builder.addCase(fetchGuestBookingHistory.fulfilled, (state, action: PayloadAction<any>) => {
+			state = { ...state, status: "sucess", bookingHistory: action.payload };
+
+			return state;
+		});
+
+		builder.addCase(fetchGuestBookingHistory.rejected, (state, action: PayloadAction<any>) => {
+			state = { ...state, status: "failed", errorMessage: action.payload };
+			return state;
+		});
+		builder.addCase(createNewBookingHistory.pending, (state) => {
+			state = { ...state, status: "loading" };
+			return state;
+		});
+		builder.addCase(createNewBookingHistory.fulfilled, (state: any, action: PayloadAction<StaffDetailsType>) => {
+			state = { ...state, staffData: [...state.staffData, action.payload], status: "success" };
+			return state;
+		});
+		builder.addCase(createNewBookingHistory.rejected, (state, action: any) => {
+			state = { ...state, status: "failed", errorMessage: action.payload };
+			// state = { ...state, status: action.payload };
+			return state;
+		});
+	},
+});
+
+export const { resetStatus } = bookingSlice.actions;
+export default bookingSlice.reducer;
