29f01de6fd9c2ea7ce2a6555911f190b97f3861b
Sena543 <senanuazanu@gmail.com>
2023-04-05 09:48:24 +0000

    booking slice setup

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import guestsReducer from "./slices/guestSlices";
 import roomReducer from "./slices/roomSlicers";
 import staffReducer from "./slices/staffSlices";
+import bookingReducer from "./slices/bookingSlices";
 
 export default configureStore({
 	reducer: {
 		guests: guestsReducer,
 		rooms: roomReducer,
 		staff: staffReducer,
+		booking: bookingReducer,
 	},
 });
