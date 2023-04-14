762bcf760a24cd137f9b9426d30ada49b73b2606
Sena543 <senanuazanu@gmail.com>
2023-04-14 16:33:16 +0000

    user data passed as props

--- src/pages/Guest/GuestDetails/GuestProfile.tsx
+++ src/pages/Guest/GuestDetails/GuestProfile.tsx
@@ -3,17 +3,18 @@ import { MailOutlineRounded, PersonRounded, PhoneOutlined } from "@mui/icons-mat
 import { Icon, Typography } from "@mui/material";
 // import imgSvg from "../../../assets/react.svg";
 import profile from "../../../assets/images/profile.jpg";
-import { useParams } from "react-router-dom";
+import { GuestsType } from "../../../constants/genericTypes";
 
-function GuestProfile() {
-	const { name } = useParams();
+function GuestProfile({ profileDetails }: { profileDetails: GuestsType }) {
 	return (
 		<div className="guest-profile-container">
 			<div className="img_name-div display-flex-flex-direction-row">
 				<img className="profile-img" loading="lazy" src={profile} alt="Guest image" />
 				<div>
 					<Typography style={{ fontSize: "15px" }}>GS-#123</Typography>
-					<Typography style={{ fontSize: "25px", fontWeight: "bold" }}>{name}</Typography>
+					<Typography
+						style={{ fontSize: "25px", fontWeight: "bold" }}
+					>{`${profileDetails.lastName} ${profileDetails.firstName}`}</Typography>
 				</div>
 			</div>
 			<div style={{ width: "70%" }}>
@@ -21,13 +22,13 @@ function GuestProfile() {
 					<Icon>
 						<PhoneOutlined />
 					</Icon>
-					<Typography>+233 123456789</Typography>
+					<Typography>{profileDetails.contact}</Typography>
 				</div>
 				<div className="display-flex-flex-direction-row">
 					<Icon>
 						<MailOutlineRounded />
 					</Icon>
-					<Typography>dude@gmail.com</Typography>
+					<Typography>{profileDetails.email}</Typography>
 				</div>
 				<div></div>
 			</div>
