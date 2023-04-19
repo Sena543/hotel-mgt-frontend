3e4f5716cdf4c4e2e082b156ec62d8450eb6bcf1
Sena <senanuazanu@gmail.com>
2023-04-19 14:45:29 +0000

    undefined error fix

--- src/pages/Guest/GuestDetails/GuestProfile.tsx
+++ src/pages/Guest/GuestDetails/GuestProfile.tsx
@@ -6,34 +6,34 @@ import profile from "../../../assets/images/profile.jpg";
 import { GuestsType } from "../../../constants/genericTypes";
 
 function GuestProfile({ profileDetails }: { profileDetails: GuestsType }) {
-	return (
-		<div className="guest-profile-container">
-			<div className="img_name-div display-flex-flex-direction-row">
-				<img className="profile-img" loading="lazy" src={profile} alt="Guest image" />
-				<div>
-					<Typography style={{ fontSize: "15px" }}>GS-#123</Typography>
-					<Typography
-						style={{ fontSize: "25px", fontWeight: "bold" }}
-					>{`${profileDetails.lastName} ${profileDetails.firstName}`}</Typography>
-				</div>
-			</div>
-			<div style={{ width: "70%" }}>
-				<div className="display-flex-flex-direction-row">
-					<Icon>
-						<PhoneOutlined />
-					</Icon>
-					<Typography>{profileDetails.contact}</Typography>
-				</div>
-				<div className="display-flex-flex-direction-row">
-					<Icon>
-						<MailOutlineRounded />
-					</Icon>
-					<Typography>{profileDetails.email}</Typography>
-				</div>
-				<div></div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="guest-profile-container">
+            <div className="img_name-div display-flex-flex-direction-row">
+                <img className="profile-img" loading="lazy" src={profile} alt="Guest image" />
+                <div>
+                    <Typography style={{ fontSize: "15px" }}>GS-#123</Typography>
+                    <Typography
+                        style={{ fontSize: "25px", fontWeight: "bold" }}
+                    >{`${profileDetails?.lastName} ${profileDetails?.firstName}`}</Typography>
+                </div>
+            </div>
+            <div style={{ width: "70%" }}>
+                <div className="display-flex-flex-direction-row">
+                    <Icon>
+                        <PhoneOutlined />
+                    </Icon>
+                    <Typography>{profileDetails?.contact}</Typography>
+                </div>
+                <div className="display-flex-flex-direction-row">
+                    <Icon>
+                        <MailOutlineRounded />
+                    </Icon>
+                    <Typography>{profileDetails?.email}</Typography>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    );
 }
 
 export default GuestProfile;
