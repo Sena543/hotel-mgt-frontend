0163999afa55bf7a08f1fc1f0f00f881d0dc4958
Sena <senanuazanu@gmail.com>
2023-04-20 12:43:13 +0000

    property undefined error fix and styling

--- src/pages/Guest/GuestDetails/GuestDetails.tsx
+++ src/pages/Guest/GuestDetails/GuestDetails.tsx
@@ -1,6 +1,15 @@
 import "./guest-details.css";
 import GenericDashCards from "../../../components/Cards/GenericDashCards";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import {
+    CircularProgress,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+} from "@mui/material";
 import GuestProfile from "./GuestProfile";
 import CurrentBooking from "./CurrentBooking";
 import { StyledTableCell, StyledTableRow } from "../../../components/Table/TableComp";
@@ -12,169 +21,183 @@ import { useEffect } from "react";
 import { fetchGuestBookingHistory } from "../../../redux/slices/bookingSlices";
 
 const prepareBookingHistory = (bookingHistory: any, roomData: any) => {
-	// bh = new booking history variable
-	let bh: any = [];
+    // bh = new booking history variable
+    let bh: any = [];
 
-	bookingHistory.forEach((historyElement: any) => {
-		roomData.forEach((roomElement: any) => {
-			if (roomElement.roomName === historyElement.roomID) {
-				bh.push({
-					...historyElement,
-					bedType: roomElement.bedType,
-					facility: roomElement.facility,
-				});
-			}
-		});
-	});
-	return bh;
+    bookingHistory.forEach((historyElement: any) => {
+        roomData.forEach((roomElement: any) => {
+            if (roomElement.roomName === historyElement.roomID) {
+                bh.push({
+                    ...historyElement,
+                    bedType: roomElement.bedType,
+                    facility: roomElement.facility,
+                });
+            }
+        });
+    });
+    return bh;
 };
 
-
-function dateWithOrdinal(n: number){return["st","nd","rd"][((n+90)%100-10)%10-1]||"th"}
+function dateWithOrdinal(n: number) {
+    return ["st", "nd", "rd"][((((n + 90) % 100) - 10) % 10) - 1] || "th";
+}
 
 function GuestDetails() {
-	const { name, guestID } = useParams();
-	const dispatch = useDispatch<AppDispatch>();
-	const { bookingHistory } = useSelector((state: any) => state.booking);
-	const { roomList: rooms } = useSelector((state: any) => state.rooms);
-	useEffect(() => {
-		dispatch(fetchGuestBookingHistory(Number(guestID)));
-	}, []);
+    const { name, guestID } = useParams();
+    const dispatch = useDispatch<AppDispatch>();
+    useEffect(() => {
+        dispatch(fetchGuestBookingHistory(Number(guestID)));
+    }, []);
 
-	//TODO write a test for the line below
-	const selectedGuestDetails = useSelector(
-		//gets the current booking of the guest
-		(state: any) => state.guests.guestsData.filter((guest: any) => guest.guestID === Number(guestID))[0]
-	);
-	const tableHeadList = ["Room Name", "Bed Type", "Room Facility", "Book Date"];
+    const { bookingHistory } = useSelector((state: any) => state.booking);
+    const { roomList: rooms } = useSelector((state: any) => state.rooms);
 
-	const preparedBookingHistoryData = prepareBookingHistory(bookingHistory, rooms);
-	console.log(preparedBookingHistoryData);
-	const months = [
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December",
-	];
+    //TODO write a test for the line below
+    const selectedGuestDetails = useSelector(
+        //gets the current booking of the guest
+        (state: any) =>
+            state.guests.guestsData.filter((guest: any) => guest.guestID === Number(guestID))[0]
+    );
+    const tableHeadList = ["Room Name", "Bed Type", "Room Facility", "Book Date"];
 
-	const dateObjFunction = (passedDate: string) => {
-		return new Date(passedDate);
-		// return new Intl.DateTimeFormat('en-gb', {month:'short'}).format(new Date(passedDate));
-	};
+    const preparedBookingHistoryData = prepareBookingHistory(bookingHistory, rooms);
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
 
-	return (
-		<div className="guest-details-container">
-			<div className="booking-history-div">
-				<Typography style={{ marginRight: "5px" }} fontSize={30} fontWeight="bold">
-					Guest Details{" "}
-				</Typography>
-			</div>
-			<div style={{ display: "flex", flexDirection: "row", marginBottom: "25px" }}>
-				<Link to={"/guests"} style={{ textDecoration: "none" }}>
-					<Typography style={{ marginRight: "3px" }}>Guest / </Typography>
-				</Link>
-				<Typography>{name}</Typography>
-			</div>
-			<div className="guest-details-booking">
-				<div className="current-booking-card">
-					<GenericDashCards>
-						<CurrentBooking bookingDetails={preparedBookingHistoryData[0]}/>
-					</GenericDashCards>
-				</div>
-				<div className="guest-profile-card">
-					<GenericDashCards>
-						<GuestProfile profileDetails={selectedGuestDetails} />
-					</GenericDashCards>
-				</div>
+    const dateObjFunction = (passedDate: string) => {
+        return new Date(passedDate);
+        // return new Intl.DateTimeFormat('en-gb', {month:'short'}).format(new Date(passedDate));
+    };
 
-				<div className="booking-history-title grid-col-span-3">
-					{/* <div className="date-report-div">
+    return (
+        <div className="guest-details-container">
+            <div className="booking-history-div">
+                <Typography style={{ marginRight: "5px" }} fontSize={30} fontWeight="bold">
+                    Guest Details{" "}
+                </Typography>
+            </div>
+            <div style={{ display: "flex", flexDirection: "row", marginBottom: "25px" }}>
+                <Link to={"/guests"} style={{ textDecoration: "none" }}>
+                    <Typography style={{ marginRight: "3px" }}>Guest / </Typography>
+                </Link>
+                <Typography>{name}</Typography>
+            </div>
+            <div className="guest-details-booking">
+                <div className="current-booking-card">
+                    <GenericDashCards>
+                        <CurrentBooking bookingDetails={preparedBookingHistoryData[0]} />
+                    </GenericDashCards>
+                </div>
+                <div className="guest-profile-card">
+                    <GenericDashCards>
+                        <GuestProfile profileDetails={selectedGuestDetails} />
+                    </GenericDashCards>
+                </div>
+
+                <div className="booking-history-title grid-col-span-3">
+                    {/* <div className="date-report-div">
 						<div>Date filter</div>
 						<div>Generate Report</div>
 					</div> */}
-				</div>
-				<div className="guest-booking-history-card grid-col-span-3">
-					<GenericDashCards>
-						<div className="booking-history-div">
-							<Typography fontWeight="bold" fontSize="20px">
-								Booking History
-							</Typography>
-						</div>
-						<TableContainer>
-							<Table>
-								<TableHead>
-									<TableRow>
-										{tableHeadList.map((name) => (
-											<StyledTableCell key={name}>{name.toLocaleUpperCase()}</StyledTableCell>
-										))}
-									</TableRow>
-								</TableHead>
-								<TableBody>
-									{/* {roomData.map( */}
-									{preparedBookingHistoryData.map(
-										({
-											roomID,
-											bedType,
-											facility,
-											status,
-											// period,
-											checkIn,
-											checkOut,
-										}: {
-											roomID: string;
-											bedType: string;
-											facility: string;
-											status: string;
-											checkIn: string;
-											checkOut: string;
-										}) => (
-											<StyledTableRow hover key={`${roomID}-${bedType}`}>
-												<TableCell>{roomID}</TableCell>
-												<TableCell>{bedType}</TableCell>
-												<TableCell>{facility}</TableCell>
-												<TableCell>
-													<Typography
-														// className={status.toLocaleLowerCase()}
-														style={{
-															display: "grid",
-															placeItems: "center",
-															width: "70%",
-															borderRadius: 5,
-														}}
-													>
-														{status}
-													</Typography>
-													<Typography variant="caption" display="block">
-														{`
-														${months[dateObjFunction(checkIn).getMonth()]} ${dateObjFunction(checkIn).getDay()}${dateWithOrdinal(dateObjFunction(checkIn).getDay())} - ${months[dateObjFunction(checkOut).getMonth()]} ${dateObjFunction(
-															checkOut
-														).getDay()}${dateWithOrdinal(dateObjFunction(checkOut).getDay())}
+                </div>
+                <div className="guest-booking-history-card grid-col-span-3">
+                    <GenericDashCards>
+                        <div className="booking-history-div">
+                            <Typography fontWeight="bold" fontSize="20px">
+                                Booking History
+                            </Typography>
+                        </div>
+                        <TableContainer>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        {tableHeadList.map((name) => (
+                                            <StyledTableCell key={name}>
+                                                {name.toLocaleUpperCase()}
+                                            </StyledTableCell>
+                                        ))}
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {/* {roomData.map( */}
+                                    {preparedBookingHistoryData.map(
+                                        ({
+                                            roomID,
+                                            bedType,
+                                            facility,
+                                            status,
+                                            // period,
+                                            checkIn,
+                                            checkOut,
+                                        }: {
+                                            roomID: string;
+                                            bedType: string;
+                                            facility: string;
+                                            status: string;
+                                            checkIn: string;
+                                            checkOut: string;
+                                        }) => (
+                                            <StyledTableRow hover key={`${roomID}-${bedType}`}>
+                                                <TableCell>{roomID}</TableCell>
+                                                <TableCell>{bedType}</TableCell>
+                                                <TableCell>{facility}</TableCell>
+                                                <TableCell>
+                                                    <Typography
+                                                        // className={status.toLocaleLowerCase()}
+                                                        style={{
+                                                            display: "grid",
+                                                            placeItems: "center",
+                                                            width: "70%",
+                                                            borderRadius: 5,
+                                                        }}
+                                                    >
+                                                        {status}
+                                                    </Typography>
+                                                    <Typography variant="caption" display="block">
+                                                        {`
+														${months[dateObjFunction(checkIn).getMonth()]} ${dateObjFunction(
+                                                            checkIn
+                                                        ).getDay()}${dateWithOrdinal(
+                                                            dateObjFunction(checkIn).getDay()
+                                                        )} - ${
+                                                            months[
+                                                                dateObjFunction(checkOut).getMonth()
+                                                            ]
+                                                        } ${dateObjFunction(
+                                                            checkOut
+                                                        ).getDay()}${dateWithOrdinal(
+                                                            dateObjFunction(checkOut).getDay()
+                                                        )}
 													`}
-													
-													{/* {
+
+                                                        {/* {
 														console.log(dateObjFunction(checkIn))
 													} */}
-													</Typography> 
-												</TableCell>
-											</StyledTableRow>
-										)
-									)}
-								</TableBody>
-							</Table>
-						</TableContainer>
-					</GenericDashCards>
-				</div>
-			</div>
-		</div>
-	);
+                                                    </Typography>
+                                                </TableCell>
+                                            </StyledTableRow>
+                                        )
+                                    )}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </GenericDashCards>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default GuestDetails;
--- src/redux/slices/bookingSlices.ts
+++ src/redux/slices/bookingSlices.ts
@@ -1,84 +1,110 @@
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getDocs, collection, addDoc, getDoc, where, query } from "firebase/firestore/lite";
+import {
+  getDocs,
+  collection,
+  addDoc,
+  getDoc,
+  where,
+  query,
+} from "firebase/firestore/lite";
 import firestoredb from "../../../firebase-config";
 import { getRawData } from "../../utils/util-functions";
 import { BookingHistoryType } from "../../constants/genericTypes";
 
 const initialState = {
-	status: "idle", // 'idle' | 'loading' | 'success'| 'failed
-	bookingHistory: [],
-	errorMessage: "",
+  status: "idle", // 'idle' | 'loading' | 'success'| 'failed
+  bookingHistory: [],
+  errorMessage: "",
 };
 
 export const fetchGuestBookingHistory = createAsyncThunk(
-	"fetch/booking-history",
-	// async function fetchGuestBookingHistory(guestID, thunkAPI) {
-	async (guestID: number, thunkAPI) => {
-		try {
-			const historyQuery = query(collection(firestoredb, "booking"), where("guestID", "==", guestID));
-			const returnedGuestsData = await getDocs(historyQuery);
-			const bookingHistoryData: any = getRawData(returnedGuestsData);
+  "fetch/booking-history",
+  // async function fetchGuestBookingHistory(guestID, thunkAPI) {
+  async (guestID: number, thunkAPI) => {
+    try {
+      const historyQuery = query(
+        collection(firestoredb, "booking"),
+        where("guestID", "==", guestID)
+      );
+      const returnedGuestsData = await getDocs(historyQuery);
+      const bookingHistoryData: any = getRawData(returnedGuestsData);
 
-			return bookingHistoryData;
-		} catch (error: any) {
-			console.log(error.message);
+      return bookingHistoryData;
+    } catch (error: any) {
+      console.log(error.message);
 
-			return thunkAPI.rejectWithValue(error.message);
-		}
-	}
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
 );
 
 export const createNewBookingHistory = createAsyncThunk(
-	"create/booking-history",
-	async function createBookingHistory(newBookingHistoryData: BookingHistoryType, thunkAPI) {
-		try {
-			await addDoc(collection(firestoredb, "booking"), newBookingHistoryData);
-			return newBookingHistoryData;
-		} catch (error: any) {
-			console.log(error);
+  "create/booking-history",
+  async function createBookingHistory(
+    newBookingHistoryData: BookingHistoryType,
+    thunkAPI
+  ) {
+    try {
+      await addDoc(collection(firestoredb, "booking"), newBookingHistoryData);
+      return newBookingHistoryData;
+    } catch (error: any) {
+      console.log(error);
 
-			return thunkAPI.rejectWithValue(error.message);
-		}
-	}
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
 );
 
 export const bookingSlice = createSlice({
-	name: "booking",
-	initialState,
-	reducers: {
-		resetStatus: (state) => {
-			state = { ...state, status: "idle" };
-		},
-	},
-	extraReducers(builder) {
-		builder.addCase(fetchGuestBookingHistory.pending, (state) => {
-			state = { ...state, status: "loading" };
-			return state;
-		});
-		builder.addCase(fetchGuestBookingHistory.fulfilled, (state, action: PayloadAction<any>) => {
-			state = { ...state, status: "sucess", bookingHistory: action.payload };
+  name: "booking",
+  initialState,
+  reducers: {
+    resetStatus: (state) => {
+      state = { ...state, status: "idle" };
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchGuestBookingHistory.pending, (state) => {
+      state = { ...state, status: "loading" };
+      return state;
+    });
+    builder.addCase(
+      fetchGuestBookingHistory.fulfilled,
+      (state, action: PayloadAction<any>) => {
+        state = { ...state, status: "sucess", bookingHistory: action.payload };
 
-			return state;
-		});
+        return state;
+      }
+    );
 
-		builder.addCase(fetchGuestBookingHistory.rejected, (state, action: PayloadAction<any>) => {
-			state = { ...state, status: "failed", errorMessage: action.payload };
-			return state;
-		});
-		builder.addCase(createNewBookingHistory.pending, (state) => {
-			state = { ...state, status: "loading" };
-			return state;
-		});
-		builder.addCase(createNewBookingHistory.fulfilled, (state: any, action: PayloadAction<BookingHistoryType>) => {
-			state = { ...state, staffData: [...state.staffData, action.payload], status: "success" };
-			return state;
-		});
-		builder.addCase(createNewBookingHistory.rejected, (state, action: any) => {
-			state = { ...state, status: "failed", errorMessage: action.payload };
-			// state = { ...state, status: action.payload };
-			return state;
-		});
-	},
+    builder.addCase(
+      fetchGuestBookingHistory.rejected,
+      (state, action: PayloadAction<any>) => {
+        state = { ...state, status: "failed", errorMessage: action.payload };
+        return state;
+      }
+    );
+    builder.addCase(createNewBookingHistory.pending, (state) => {
+      state = { ...state, status: "loading" };
+      return state;
+    });
+    builder.addCase(
+      createNewBookingHistory.fulfilled,
+      (state: any, action: PayloadAction<BookingHistoryType>) => {
+        state = {
+          ...state,
+          staffData: [...state.staffData, action.payload],
+          status: "success",
+        };
+        return state;
+      }
+    );
+    builder.addCase(createNewBookingHistory.rejected, (state, action: any) => {
+      state = { ...state, status: "failed", errorMessage: action.payload };
+      // state = { ...state, status: action.payload };
+      return state;
+    });
+  },
 });
 
 export const { resetStatus } = bookingSlice.actions;
