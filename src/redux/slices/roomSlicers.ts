1468a7214d2195403977032c8341d14856bb39d7
Sena <senanuazanu@gmail.com>
2023-04-20 14:43:27 +0000

    fix: bug fix for setting room  availability when no guests are available

--- src/redux/slices/roomSlicers.ts
+++ src/redux/slices/roomSlicers.ts
@@ -4,62 +4,68 @@ import firestoredb from "../../../firebase-config";
 import { getRawData } from "../../utils/util-functions";
 
 const initialState = {
-	status: "idle", // 'idle' | 'loading' | 'success'| 'failed
-	roomList: [],
-	errorMessage: "",
+    status: "idle", // 'idle' | 'loading' | 'success'| 'failed
+    roomList: [],
+    errorMessage: "",
 };
 
 export const fetchAllRooms = createAsyncThunk("fetch/rooms", async (_, thunkAPI) => {
-	try {
-		const returnedRoomsData = await getDocs(collection(firestoredb, "rooms"));
-		const returnedGuestsData = await getDocs(collection(firestoredb, "guests"));
-		let roomsData: any = getRawData(returnedRoomsData);
-		let guestsData: any = getRawData(returnedGuestsData);
+    try {
+        const returnedRoomsData = await getDocs(collection(firestoredb, "rooms"));
+        const returnedGuestsData = await getDocs(collection(firestoredb, "guests"));
+        let roomsData: any = getRawData(returnedRoomsData);
+        let guestsData: any = getRawData(returnedGuestsData);
 
-		return { roomsData, guestsData };
-	} catch (error: any) {
-		console.log(error.message);
+        return { roomsData, guestsData };
+    } catch (error: any) {
+        console.log(error.message);
 
-		return thunkAPI.rejectWithValue(error.message);
-	}
+        return thunkAPI.rejectWithValue(error.message);
+    }
 });
 export const roomSlice = createSlice({
-	name: "rooms",
-	initialState,
-	// initialState: [],
-	reducers: {
-		resetStatus: (state) => {
-			state = { ...state, status: "idle" };
-		},
-	},
-	extraReducers: (builder) => {
-		builder.addCase(fetchAllRooms.pending, (state) => {
-			state = { ...state, status: "loading" };
-			return state;
-		});
-		builder.addCase(fetchAllRooms.fulfilled, (state, action: PayloadAction<any>) => {
-			const { roomsData, guestsData } = action.payload;
+    name: "rooms",
+    initialState,
+    // initialState: [],
+    reducers: {
+        resetStatus: (state) => {
+            state = { ...state, status: "idle" };
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchAllRooms.pending, (state) => {
+            state = { ...state, status: "loading" };
+            return state;
+        });
+        builder.addCase(fetchAllRooms.fulfilled, (state, action: PayloadAction<any>) => {
+            const { roomsData, guestsData } = action.payload;
 
-			//assign status of room to each room by looping over guests
-			//and checking if roomName == roomAssigned
-			roomsData.forEach((room: any) => {
-				guestsData.forEach((guest: any) => {
-					if (room.roomName === guest.roomAssigned) {
-						room["status"] = "Booked";
-					} else {
-						room["status"] = "Available";
-					}
-				});
-			});
-			state = { ...state, roomList: roomsData, status: "success" };
-			return state;
-		});
+            //assign status of room to each room by looping over guests
+            //and checking if roomName == roomAssigned
 
-		builder.addCase(fetchAllRooms.rejected, (state, action: PayloadAction<any>) => {
-			state = { ...state, status: "failed", errorMessage: action.payload };
-			return state;
-		});
-	},
+            roomsData.forEach((room: any) => {
+                guestsData.forEach((guest: any) => {
+                    if (room.roomName === guest.roomAssigned) {
+                        room["status"] = "Booked";
+                    } else {
+                        room["status"] = "Available";
+                    }
+                });
+            });
+            if (guestsData.length === 0) {
+                roomsData.forEach((room: any) => {
+                    room["status"] = "Available";
+                });
+            }
+            state = { ...state, roomList: roomsData, status: "success" };
+            return state;
+        });
+
+        builder.addCase(fetchAllRooms.rejected, (state, action: PayloadAction<any>) => {
+            state = { ...state, status: "failed", errorMessage: action.payload };
+            return state;
+        });
+    },
 });
 
 export const { resetStatus } = roomSlice.actions;
