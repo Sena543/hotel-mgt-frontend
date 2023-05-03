cd13dc6ca5fa0ab8c3192a58c94e6a7c2b49964b
Sena <senanuazanu@gmail.com>
2023-05-03 12:55:35 +0000

    toast setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,31 @@ import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./themes/theme";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-	return (
-		<Provider store={store}>
-			<ThemeProvider theme={theme}>
-				<div className="App">
-					<RouterProvider router={routes} />
-				</div>
-			</ThemeProvider>
-		</Provider>
-	);
+    return (
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <div className="App">
+                    <RouterProvider router={routes} />
+                </div>
+            </ThemeProvider>
+            <ToastContainer
+                position="bottom-center"
+                autoClose={4000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+        </Provider>
+    );
 }
 
 export default App;
