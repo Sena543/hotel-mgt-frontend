f21d688ed4469eb616520dde216e4a7e15afe0b9
Sena <senanuazanu@gmail.com>
2023-05-05 13:34:23 +0000

    minor change

--- src/components/dashboard/GlanceCards.tsx
+++ src/components/dashboard/GlanceCards.tsx
@@ -2,20 +2,24 @@ import { Typography } from "@mui/material";
 import "./styles/glance.css";
 
 type GlanceCardsProps = {
-	number: number;
-	name: string;
-	icon: JSX.Element;
-	backgColor: string;
+    number: number;
+    name: string;
+    icon: JSX.Element;
+    backgColor: string;
 };
 
 export const GlanceCards = ({ number, name, icon, backgColor }: GlanceCardsProps) => {
-	return (
-		<div className="glance-container" style={{ backgroundColor: backgColor }}>
-			<div>
-				<Typography variant="h5">{number}</Typography>
-				<Typography variant="caption">{name}</Typography>
-			</div>
-			<div>{icon}</div>
-		</div>
-	);
+    return (
+        <div
+            key={`${name}${backgColor}`}
+            className="glance-container"
+            style={{ backgroundColor: backgColor }}
+        >
+            <div>
+                <Typography variant="h5">{number}</Typography>
+                <Typography variant="caption">{name}</Typography>
+            </div>
+            <div>{icon}</div>
+        </div>
+    );
 };
