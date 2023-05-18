5e7d6912e181e58c1a167fa89623573401a274e0
Sena <senanuazanu@gmail.com>
2023-05-18 11:38:37 +0000

    image styling

--- src/pages/Guest/GuestDetails/CarouselImage.tsx
+++ src/pages/Guest/GuestDetails/CarouselImage.tsx
@@ -1,21 +1,27 @@
-import React from "react";
-
+import "./carouselImage.css";
 function CarouselImage({ index, imgUrl, key }: { index: number; imgUrl: string; key?: string }) {
-	return (
-		<div
-			key={`${imgUrl}-${index}`}
-			style={{
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-				height: "300px",
-				width: "100%",
-				margin: "10px",
-			}}
-		>
-			<img style={{ borderRadius: "10px" }} loading="lazy" src={imgUrl} alt="room sample" />
-		</div>
-	);
+    return (
+        <div
+            key={`${imgUrl}-${index}`}
+            className="carousel-image-div"
+            // style={{
+            //     display: "flex",
+            //     justifyContent: "center",
+            //     alignItems: "center",
+            //     height: "300px",
+            //     width: "100%",
+            //     // margin: "10px",
+            // }}
+        >
+            <img
+                className="carousel-image"
+                // style={{ borderRadius: "10px" }}
+                loading="lazy"
+                src={imgUrl}
+                alt="room sample"
+            />
+        </div>
+    );
 }
 
 export default CarouselImage;
