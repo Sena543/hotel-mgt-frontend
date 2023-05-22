504f6c15d2ffa178dca0ecfbc2f2fc9df0644e02
Sena <senanuazanu@gmail.com>
2023-05-22 11:42:19 +0000

    app file test

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import App from "./App";
+import { renderWithProviders } from "./test/test-utils/redux-render-provider";
+
+describe("App Test", () => {
+    test("if test setup works properly", () => {
+        renderWithProviders(<App />);
+        expect(screen.getByTestId("hello")).toBeDefined();
+    });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     return (
         <Provider store={store}>
             <ThemeProvider theme={theme}>
-                <div className="App">
+                <div className="App" data-testid="hello">
                     <RouterProvider router={routes} />
                 </div>
             </ThemeProvider>
