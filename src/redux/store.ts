d6ec05b6ef7fc38f2e3d441777bf57be2781aecd
Sena <senanuazanu@gmail.com>
2023-05-22 11:41:48 +0000

    store setup fn for testing

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { PreloadedState, configureStore } from "@reduxjs/toolkit";
 import guestsReducer from "./slices/guestSlices";
 import roomReducer from "./slices/roomSlicers";
 import staffReducer from "./slices/staffSlices";
 import bookingReducer from "./slices/bookingSlices";
 import restaurantReducer from "./slices/restaurantSlice";
+import { RootState } from "./types";
+
+export function setupStore(preloadedState?: PreloadedState<RootState>) {
+    return configureStore({
+        reducer: {
+            guests: guestsReducer,
+            rooms: roomReducer,
+            staff: staffReducer,
+            booking: bookingReducer,
+            restaurant: restaurantReducer,
+        },
+        preloadedState,
+    });
+}
 
 export default configureStore({
-	reducer: {
-		guests: guestsReducer,
-		rooms: roomReducer,
-		staff: staffReducer,
-		booking: bookingReducer,
-		restaurant: restaurantReducer,
-	},
+    reducer: {
+        guests: guestsReducer,
+        rooms: roomReducer,
+        staff: staffReducer,
+        booking: bookingReducer,
+        restaurant: restaurantReducer,
+    },
 });
--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,5 +1,6 @@
-import store from "./store";
+import store, { setupStore } from "./store";
 
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export type AppStore = ReturnType<typeof setupStore>;
