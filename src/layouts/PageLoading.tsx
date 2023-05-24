423fe49105bfe55486873b4c04377d8f5c06a72d
Sena <senanuazanu@gmail.com>
2023-05-24 10:37:44 +0000

    added circular progress

--- src/layouts/PageLoading.tsx
+++ src/layouts/PageLoading.tsx
@@ -1,7 +1,12 @@
+import { CircularProgress } from "@mui/material";
 import React from "react";
 
 function PageLoading() {
-	return <div>Loading</div>;
+    return (
+        <div style={{ width: "100%", height: "100%", display: "grid", placeContent: "center" }}>
+            <CircularProgress size={80} />
+        </div>
+    );
 }
 
 export default PageLoading;
