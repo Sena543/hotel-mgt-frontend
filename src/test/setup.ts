74308a6241f854a9cb5db2a3af1dc8b8dcc7479e
Sena <senanuazanu@gmail.com>
2023-05-25 14:32:07 +0000

    firebase emulator setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -5,34 +5,52 @@ import { rest } from "msw";
 import { setupServer } from "msw/node";
 import { roomData } from "../services/roomList";
 
-// extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
 
 const handlers = [
     rest.get(
-        "https://firestore.googleapis.com/v1/projects/hotel-management-system-dev/databases/(default)/documents:runQuery",
-        // "fetch/rooms",
+        // "localhost:4000/firestore",
+        "http://localhost:4000/firestore/data/rooms/",
+        // "http://localhost:4000/v1/projects/hotel-management-system-dev/databases/(default)/documents",
         (req, res, ctx) => {
+            console.log("Get Req", req);
             return res(ctx.json(roomData), ctx.delay(150));
         }
     ),
+    rest.post(
+        // "localhost:4000/firestore",
+        "fetch/rooms",
+        (req, res, ctx) => {
+            console.log("Post Req", req);
+            return res(ctx.json(roomData), ctx.delay(150));
+        }
+    ),
+    rest.get(
+        "fetch/guests",
+        // "localhost:4000/firestore",
+        // "http://localhost:4000/firestore/data/rooms/",
+        (req, res, ctx) => {
+            console.log("Get Req", req);
+            return res(ctx.json(roomData), ctx.delay(150));
+        }
+    ),
+    rest.post("post/new-guests", (req, res, ctx) => {
+        console.log("Post Req", req);
+        return res(ctx.json(roomData), ctx.delay(150));
+    }),
 ];
 
 const server = setupServer(...handlers);
 
-// // Enable API mocking before tests.
 beforeAll(() => {
     console.log("Before all");
     server.listen();
 });
 
-// // Reset any runtime request handlers we may add during the tests.
 afterEach(() => server.resetHandlers());
 
-// // Disable API mocking after the tests are done.
 afterAll(() => server.close());
 
-// runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
     cleanup();
 });
