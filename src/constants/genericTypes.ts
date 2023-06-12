985b8b1d559b00770d5bbf1ad406078d6c91f8b9
Sena <senanuazanu@gmail.com>
2023-06-12 12:26:35 +0000

    minor changes - raw doc id and special request optional

--- src/constants/genericTypes.ts
+++ src/constants/genericTypes.ts
@@ -1,40 +1,41 @@
 export type GuestsType = {
-	lastName: string;
-	firstName: string;
-	roomAssigned: string;
-	checkIn: string | any;
-	checkOut: string | any;
-	specialRequests: string;
-	status?: string;
-	email: string;
-	contact: string;
+    lastName: string;
+    firstName: string;
+    roomAssigned: string;
+    checkIn: string | any;
+    checkOut: string | any;
+    specialRequests?: string;
+    status?: string;
+    email: string;
+    contact: string;
 };
 
 export type StaffDetailsType = {
-	lastName: string;
-	firstName: string;
-	email: string;
-	contact: string;
-	jobDescription: string;
-	jobTitle: string;
-	workingDays: string[];
-	employeeID: string;
+    lastName: string;
+    firstName: string;
+    email: string;
+    contact: string;
+    jobDescription: string;
+    jobTitle: string;
+    workingDays: string[];
+    employeeID: string;
 };
 
 export type MenuItemType = {
-	rawDocID?: string;
-	menuType: string;
-	dishOrBev: string;
-	price: string;
-	dishType: string;
-	description: string;
+    rawDocID?: string;
+    menuType: string;
+    dishOrBev: string;
+    price: string;
+    dishType: string;
+    description: string;
 };
 
 export type BookingHistoryType = {
-	bookingID: string;
-	guestID: string;
-	roomID: string;
-	checkIn: string;
-	checkOut: string;
-	mealOrderID?: string[];
+    bookingID: string;
+    guestID: string;
+    roomID: string;
+    checkIn: string;
+    checkOut: string;
+    mealOrderID?: string[];
+    rawDocID?: string;
 };
