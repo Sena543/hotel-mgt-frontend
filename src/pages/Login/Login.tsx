842d3603e18c8ae1f7fcd8f941a1f2f416de8512
Sena <senanuazanu@gmail.com>
2023-07-03 13:52:49 +0000

    login page almost done

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,62 @@
+import "./login.css";
+import { Button, IconButton, InputAdornment, Typography } from "@mui/material";
+import CustomTextField from "../../components/TextInput/CustomTextField";
+import { VisibilityOff, Visibility } from "@mui/icons-material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword((showPassword) => !showPassword);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+    return (
+        <div className="login-root">
+            <div>
+                <div>
+                    <Typography variant="h3">Login</Typography>
+                </div>
+                {/* <div> */}
+                <div className="login-form">
+                    <CustomTextField
+                        autoFocus
+                        name="useremail"
+                        variant="outlined"
+                        className="custom-text-field"
+                        label="Email"
+                        margin="normal"
+                    />
+                    <CustomTextField
+                        name="password"
+                        variant="outlined"
+                        className="custom-text-field"
+                        label="Password"
+                        margin="normal"
+                        type={showPassword ? "text" : "password"}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <Button variant="contained">Login</Button>
+                </div>
+                <a href="google.com">Google</a>
+                {/* <Link to={"sign-up"}>Sign up</Link> */}
+            </div>
+        </div>
+    );
+}
+
+export default Login;
