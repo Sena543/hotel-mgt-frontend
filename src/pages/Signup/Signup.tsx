8dfa966581219e135fe0e3f8a70fef6221b9f94a
Sena <senanuazanu@gmail.com>
2023-07-14 11:47:59 +0000

    signup page link set up

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Signup() {
-    return <div>Signup</div>;
+    return (
+        <div>
+            Signup Page
+            <Link to={"/sign-in"}>Login</Link>
+        </div>
+    );
 }
 
 export default Signup;
