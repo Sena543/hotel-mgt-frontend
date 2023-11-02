87be4c562b6a6e1d6cb19dc63776cb9b7d0090cf
Sena543 <senanuazanu@gmail.com>
2023-11-02 20:10:03 +0000

    admin

---
+++ src/components/Admin/AdminTable.tsx
@@ -0,0 +1,71 @@
+import { TableContainer, Table, TableHead, TableRow, TableBody } from "@mui/material";
+import React from "react";
+import { StyledTableCell, StyledTableRow } from "../Table/TableComp";
+
+function AdminTable() {
+	const header = ["Item No", "Item Name", "Location", "Quantity Available", "Category"];
+	const newHeader = ["Full Name", "Email", "Role"];
+	const tableData = [
+		{
+			name: "Chocolate",
+			itemNo: "1",
+			location: "Warehouse 1 A1R4",
+			quantityAvailable: "50",
+			itemCategory: "Food",
+		},
+	];
+	return (
+		<TableContainer>
+			<Table>
+				<TableHead>
+					<TableRow>
+						{header &&
+							header.map((name) => (
+								<StyledTableCell key={name}>{name.toLocaleUpperCase()}</StyledTableCell>
+							))}
+						{/* {showActionCol ? <StyledTableCell>ACTIONS</StyledTableCell> : null} */}
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{tableData &&
+						tableData.map(
+							(
+								{
+									itemNo,
+									location,
+									quantityAvailable,
+									itemCategory,
+									name,
+								}: {
+									itemNo: string;
+									location: string;
+									quantityAvailable: string;
+									itemCategory: string;
+									name: string;
+								},
+								index: number
+							) => (
+								<StyledTableRow key={`${index}`} hover>
+									<StyledTableCell>
+										{/* <Link
+                                    to={`/${itemNo}-${name}`}
+                                    style={{ textDecoration: "none" }}
+                                >
+                                    {itemNo}
+                                </Link> */}
+										{itemNo}
+									</StyledTableCell>
+									<StyledTableCell>{name}</StyledTableCell>
+									<StyledTableCell>{location}</StyledTableCell>
+									<StyledTableCell>{quantityAvailable}</StyledTableCell>
+									<StyledTableCell>{itemCategory}</StyledTableCell>
+								</StyledTableRow>
+							)
+						)}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+}
+
+export default AdminTable;
