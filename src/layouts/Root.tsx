ea8b857d39744c95769c9e00c8a97d39528e4162
Sena543 <senanuazanu@gmail.com>
2023-11-02 19:47:48 +0000

    setting up protection

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,24 +1,30 @@
 import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import PageLoading from "./PageLoading";
 import Sidebar from "./Sidebar/Sidebar";
 import Login from "../pages/Login/Login";
 import Signup from "../pages/Signup/Signup";
+import { useSelector } from "react-redux";
 
 function RootLayout() {
-    const signedIn = true;
-    return (
-        <>
-            {signedIn ? (
-                <Suspense fallback={<PageLoading />}>
-                    <Sidebar />
-                    <Outlet />
-                </Suspense>
-            ) : (
-                <Login />
-            )}
-        </>
-    );
+	// const signedIn = localStorage.getItem("role");
+	const navigate = useNavigate();
+	const user = useSelector((state: any) => state.auth.authData);
+	if (user.email.length === 0) {
+		navigate("/login");
+	}
+	return (
+		<>
+			{/* {signedIn ? ( */}
+			<Suspense fallback={<PageLoading />}>
+				<Sidebar />
+				<Outlet />
+			</Suspense>
+			{/* ) : ( */}
+			{/* <Login /> */}
+			{/* )} */}
+		</>
+	);
 }
 
 export default RootLayout;
