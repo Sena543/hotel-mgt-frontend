bcad06dc14d6bf5c8a6c66aa3125fc9b2f6a6687
Sena543 <senanuazanu@gmail.com>
2023-11-02 19:46:27 +0000

    auth config

--- firebase-config.ts
+++ firebase-config.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,6 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 const firestoredb = getFirestore(app);
 export default firestoredb;
 // const analytics = getAnalytics(app);
