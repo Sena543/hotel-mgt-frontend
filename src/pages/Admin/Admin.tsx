e30fb3bce0c477766e38a48ba61c7fd6028efba3
Sena543 <senanuazanu@gmail.com>
2023-11-02 20:10:23 +0000

    admin

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,7 +1,26 @@
+import { Typography, Button } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
+import AdminTable from "../../components/Admin/AdminTable";
 
 function Admin() {
-	return <div>Admin</div>;
+	return (
+		<div>
+			<div className="restaurant-header">
+				<Typography style={{}} fontSize={30} fontWeight="bold">
+					Administrator Panel
+				</Typography>
+
+				{/* <div className="header-button-div">
+					<Link className="menu-link" to={"/restaurant/menu"}>
+						<Button>View Menu</Button>
+					</Link>
+					<Button onClick={() => {}}>Add Item</Button>
+				</div> */}
+			</div>
+			<AdminTable />
+		</div>
+	);
 }
 
 export default Admin;
