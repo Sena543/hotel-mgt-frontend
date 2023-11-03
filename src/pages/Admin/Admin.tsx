eeddaecfbc222f2c0a038d3743023c5c445ff4b6
Sena <senanuazanu@gmail.com>
2023-11-03 10:13:36 +0000

    fix

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -2,28 +2,29 @@ import { Typography, Button } from "@mui/material";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import AdminTable from "../../components/Admin/AdminTable";
-import CreateNewUser from "../../components/Admin/CreateNewUSer";
+import CreateNewUser from "../../components/Admin/CreateNewUser";
 
 function Admin() {
-	const [openModal, setOpenModal] = useState(false);
-	return (
-		<div>
-			<div className="restaurant-header">
-				<Typography style={{}} fontSize={30} fontWeight="bold">
-					Administrator Panel
-				</Typography>
+    const [openModal, setOpenModal] = useState(false);
+    return (
+        <div>
+            <div className="restaurant-header">
+                <Typography style={{}} fontSize={30} fontWeight="bold">
+                    Administrator Panel
+                </Typography>
 
-				<div className="header-button-div">
-					{/* <Link className="menu-link" to={"/restaurant/menu"}>
+                <div className="header-button-div">
+                    {/* <Link className="menu-link" to={"/restaurant/menu"}>
 						<Button>View Menu</Button>
 					</Link> */}
-					<Button onClick={() => setOpenModal(true)}>Create New User</Button>
-				</div>
-			</div>
-			<AdminTable />
-			<CreateNewUser open={openModal} setOpenModal={setOpenModal} />
-		</div>
-	);
+                    <Button onClick={() => setOpenModal(true)}>Create New User</Button>
+                </div>
+            </div>
+            <AdminTable />
+            {/* <CreateNewUser open={openModal} setOpenModal={setOpenModal} /> */}
+            <CreateNewUser open={openModal} setOpenModal={setOpenModal} />
+        </div>
+    );
 }
 
 export default Admin;
