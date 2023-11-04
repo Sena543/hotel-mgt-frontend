c3ca9ae1f3b20b2ce412123a7307ce1ea68e3359
Sena543 <senanuazanu@gmail.com>
2023-11-04 18:32:22 +0000

    upload room image working

--- src/redux/slices/roomSlicers.ts
+++ src/redux/slices/roomSlicers.ts
@@ -1,92 +1,143 @@
-import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getDocs, collection, QuerySnapshot, DocumentData } from "firebase/firestore/lite";
+import { PayloadAction, createAsyncThunk, createSlice, current } from "@reduxjs/toolkit";
+import { arrayUnion, doc, updateDoc, getDocs, collection, QuerySnapshot, DocumentData } from "firebase/firestore/lite";
 import firestoredb from "../../../firebase-config";
-import { getRawData } from "../../utils/util-functions";
+import { createUploadRef, getRawData } from "../../utils/util-functions";
 import dayjs from "dayjs";
 import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
 import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
 import { toast } from "react-toastify";
 import { GuestsType } from "../../constants/genericTypes";
 import { RoomType } from "../../components/Room/RoomList";
+import { getDownloadURL, uploadBytes } from "firebase/storage";
+// import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 dayjs.extend(isSameOrAfter);
 dayjs.extend(isSameOrBefore);
 
 type RoomStateType = {
-    status: "idle" | "loading" | "success" | "failed";
-    roomList: RoomType[];
-    errorMessage: string;
+	status: "idle" | "loading" | "success" | "failed";
+	roomList: RoomType[];
+	errorMessage: string;
 };
 
 const initialState: RoomStateType = {
-    status: "idle", // 'idle' | 'loading' | 'success'| 'failed
-    roomList: [],
-    errorMessage: "",
+	status: "idle", // 'idle' | 'loading' | 'success'| 'failed
+	roomList: [],
+	errorMessage: "",
 };
 
 export const fetchAllRooms = createAsyncThunk("fetch/rooms", async (_, thunkAPI) => {
-    try {
-        const returnedRoomsData = await getDocs(collection(firestoredb, "rooms"));
-        const returnedGuestsData = await getDocs(collection(firestoredb, "guests"));
-        let roomsData = getRawData<RoomType>(returnedRoomsData);
-        let guestsData = getRawData<GuestsType>(returnedGuestsData);
+	try {
+		const returnedRoomsData = await getDocs(collection(firestoredb, "rooms"));
+		const returnedGuestsData = await getDocs(collection(firestoredb, "guests"));
+		let roomsData = getRawData<RoomType>(returnedRoomsData);
+		let guestsData = getRawData<GuestsType>(returnedGuestsData);
 
-        return { roomsData, guestsData };
-    } catch (error: any) {
-        console.log(await error);
+		return { roomsData, guestsData };
+	} catch (error: any) {
+		console.log(await error);
 
-        return thunkAPI.rejectWithValue(error.message);
-    }
+		return thunkAPI.rejectWithValue(error.message);
+	}
 });
 
+export const addRoomImage = createAsyncThunk(
+	"post/roomImage",
+	async (uploadDetailds: { file: File; roomDetails: RoomType }, thunkAPI) => {
+		try {
+			const { file, roomDetails } = uploadDetailds;
+			console.log(roomDetails);
+			const imageRef = createUploadRef("/images", file.name);
+			// const returnedRoomsData = await getDocs(collection(firestoredb, "rooms"));
+			const returnedValue = await uploadBytes(imageRef, file);
+			const imgUrl = await getDownloadURL(returnedValue.ref);
+			const { rawDocID } = roomDetails;
+			console.log(rawDocID, "mmm");
+			const docRef = doc(firestoredb, "rooms", rawDocID);
+			await updateDoc(docRef, {
+				imageUrls: arrayUnion(imgUrl),
+			});
+			return { imgUrl, roomDetails };
+		} catch (error: any) {
+			console.log(await error);
+			console.error(error);
+			return thunkAPI.rejectWithValue(error.message);
+		}
+	}
+);
+
 export const roomSlice = createSlice({
-    name: "rooms",
-    initialState,
-    // initialState: [],
-    reducers: {
-        resetRoomStatus: (state) => {
-            state = { ...state, status: "idle" };
-        },
-    },
-    extraReducers: (builder) => {
-        builder.addCase(fetchAllRooms.pending, (state) => {
-            state = { ...state, status: "loading" };
-            return state;
-        });
-        builder.addCase(fetchAllRooms.fulfilled, (state, action) => {
-            const { roomsData, guestsData } = action.payload;
-            let availableRooms = roomsData.map((room) => room.roomName);
+	name: "rooms",
+	initialState,
+	// initialState: [],
+	reducers: {
+		resetRoomStatus: (state) => {
+			state = { ...state, status: "idle" };
+		},
+	},
+	extraReducers: (builder) => {
+		builder.addCase(fetchAllRooms.pending, (state) => {
+			state = { ...state, status: "loading" };
+			return state;
+		});
+		builder.addCase(fetchAllRooms.fulfilled, (state, action) => {
+			const { roomsData, guestsData } = action.payload;
+			let availableRooms = roomsData.map((room) => room.roomName);
 
-            // Iterate over all the guests and remove any rooms from
-            // the availableRooms array that are occupied
-            guestsData.forEach((guest) => {
-                if (
-                    availableRooms.includes(guest.roomAssigned) &&
-                    dayjs().isBefore(dayjs(guest.checkOut))
-                ) {
-                    // Remove the occupied room from the available list
-                    availableRooms = availableRooms.filter((room) => room !== guest.roomAssigned);
-                }
-            });
+			// Iterate over all the guests and remove any rooms from
+			// the availableRooms array that are occupied
+			guestsData.forEach((guest) => {
+				if (availableRooms.includes(guest.roomAssigned) && dayjs().isBefore(dayjs(guest.checkOut))) {
+					// Remove the occupied room from the available list
+					availableRooms = availableRooms.filter((room) => room !== guest.roomAssigned);
+				}
+			});
 
-            // Update the status of each room based on those that are available
-            // and those that are not.
-            roomsData.forEach((room) => {
-                if (availableRooms.includes(room.roomName)) {
-                    room["status"] = "Available";
-                } else {
-                    room["status"] = "Booked";
-                }
-            });
-            state = { ...state, roomList: roomsData, status: "success" };
-            return state;
-        });
+			// Update the status of each room based on those that are available
+			// and those that are not.
+			roomsData.forEach((room) => {
+				if (availableRooms.includes(room.roomName)) {
+					room["status"] = "Available";
+				} else {
+					room["status"] = "Booked";
+				}
+			});
+			state = { ...state, roomList: roomsData, status: "success" };
+			return state;
+		});
 
-        builder.addCase(fetchAllRooms.rejected, (state, action: PayloadAction<any>) => {
-            state = { ...state, status: "failed", errorMessage: action.payload };
-            toast.warn(`Fetch Room Error: ${state.errorMessage}`);
-            return state;
-        });
-    },
+		builder.addCase(fetchAllRooms.rejected, (state, action: PayloadAction<any>) => {
+			state = { ...state, status: "failed", errorMessage: action.payload };
+			toast.warn(`Fetch Room Error: ${state.errorMessage}`);
+			return state;
+		});
+		builder.addCase(addRoomImage.pending, (state, action: PayloadAction<any>) => {
+			state = { ...state, status: "loading" };
+			return state;
+		});
+		builder.addCase(addRoomImage.fulfilled, (state, action: PayloadAction<any>) => {
+			const { imgUrl, roomDetails } = action.payload;
+			// console.log(roomDetails);
+			let currentState = JSON.parse(JSON.stringify(current(state))); //makes the state mutable
+			const findRoom = currentState.roomList.filter((room: RoomType) => {
+				return room.rawDocID === roomDetails.rawDocID;
+			})[0];
+			let roomList = currentState.roomList.filter((room: RoomType) => {
+				return room.rawDocID != roomDetails.rawDocID;
+			});
+			findRoom.imageUrls.push(imgUrl);
+			roomList = [...roomList, findRoom];
+			currentState.roomList = roomList;
+			// state = { ...state, roomList: roomList, status: "success" };
+			toast.success(`Image Upload success`);
+			// return state;
+			return currentState;
+		});
+		builder.addCase(addRoomImage.rejected, (state, action: PayloadAction<any>) => {
+			state = { ...state, status: "failed", errorMessage: action.payload };
+			toast.warn(`Fetch Room Error: ${state.errorMessage}`);
+			return state;
+		});
+	},
 });
 
 export const { resetRoomStatus: resetStatus } = roomSlice.actions;
