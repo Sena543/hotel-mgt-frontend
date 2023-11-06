73532279beedf79b9aa2868b287c127beb5b7d9b
Sena <senanuazanu@gmail.com>
2023-11-06 12:17:52 +0000

    body and root css changes

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -4,17 +4,19 @@ import PageLoading from "./PageLoading";
 import Sidebar from "./Sidebar/Sidebar";
 
 function RootLayout() {
-	if (!localStorage.getItem("role")) {
-		return <Navigate to="sign-in" />;
-	}
-	return (
-		<>
-			<Suspense fallback={<PageLoading />}>
-				<Sidebar />
-				<Outlet />
-			</Suspense>
-		</>
-	);
+    if (!localStorage.getItem("role")) {
+        return <Navigate to="sign-in" />;
+    }
+    return (
+        <div style={{ margin: "0 auto", padding: "2rem" }}>
+            <Suspense fallback={<PageLoading />}>
+                <Sidebar />
+                <div style={{marginLeft:'6%'}}>
+                    <Outlet />
+                </div>
+            </Suspense>
+        </div>
+    );
 }
 
 export default RootLayout;
