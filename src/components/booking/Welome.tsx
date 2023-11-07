b4b94cfa4695c8ecc8aa8814749a23f27d5cf25a
Sena <senanuazanu@gmail.com>
2023-11-07 12:38:43 +0000

    minor change

--- src/components/booking/Welome.tsx
+++ src/components/booking/Welome.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import welcomeImage from "../../assets/images/welcome-hotel.avif";
 
 export default function Welcome() {
-	return <div className="welcome-div">Welcome</div>;
+    return (
+        <div className="welcome-div">
+            {/* <img src={welcomeImage} loading="lazy" height={"100%"} width={'100%'} /> */}
+        </div>
+    );
 }
