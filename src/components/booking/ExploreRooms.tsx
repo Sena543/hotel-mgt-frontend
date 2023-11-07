a0e67b527f58d523e48bc580686f2736c2ab06e4
Sena <senanuazanu@gmail.com>
2023-11-07 16:39:11 +0000

    explorer rooms

--- src/components/booking/ExploreRooms.tsx
+++ src/components/booking/ExploreRooms.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import welcomeImage from "../../assets/images/welcome-hotel.avif";
+import { Typography } from "@mui/material";
+
+const RoomCard = ({ type, description }: { type: string; description: string }) => {
+    return (
+        <div className="room-card">
+            <div style={{ minHeight: "150px" }} className="room-card-image-div">
+                <img src={welcomeImage} loading="lazy" height={"100%"} width={"100%"} />
+            </div>
+            <div style={{ minHeight: "150px" }}>
+                <Typography>{type}</Typography>
+                <Typography>{description}</Typography>
+            </div>
+        </div>
+    );
+};
 
 function ExploreRooms() {
-  return (
-    <div>ExploreRooms</div>
-  )
+    const roomsTypes = [
+        {
+            type: "Lorem Ipsum Room",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+        },
+        {
+            type: "Lorem Ipsum Room",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+        },
+        {
+            type: "Lorem Ipsum Room",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+        },
+    ];
+    return (
+        <div className="booking-explore-rooms-section">
+            <div style={{ display: "flex", justifyContent: "flex-start", margin: "2%" }}>
+                <Typography variant="h4">Rooms & Suites</Typography>
+            </div>
+
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-around",
+                    marginTop: "5px",
+                }}
+            >
+                {roomsTypes.map((roomType) => (
+                    <RoomCard type={roomType.type} description={roomType.description} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default ExploreRooms
+export default ExploreRooms;
