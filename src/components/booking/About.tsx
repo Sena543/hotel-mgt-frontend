a5a392627c5c6b0889bbbe3a3bd765e8350ed037
Sena <senanuazanu@gmail.com>
2023-11-07 12:56:38 +0000

    about hotel almost done

--- src/components/booking/About.tsx
+++ src/components/booking/About.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import welcomeImage from "../../assets/images/welcome-hotel.avif";
+import { Typography } from "@mui/material";
 
 function About() {
-  return (
-    <div>About</div>
-  )
+    return (
+        <div className="booking-about-section">
+            <div className="about-div">
+                <div>
+                    <Typography variant="h4">About Hotel</Typography>
+                </div>
+                <div>
+                    <Typography>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus cum
+                        fugit excepturi temporibus nostrum officia debitis autem cumque soluta?
+                        Molestiae nostrum sequi maxime minima quo harum tempora aspernatur deserunt
+                        possimus!
+                    </Typography>
+                </div>
+            </div>
+            <div className="about-image-div">
+                <img src={welcomeImage} loading="lazy" height={"100%"} width={"100%"} />
+            </div>
+        </div>
+    );
 }
 
-export default About
+export default About;
