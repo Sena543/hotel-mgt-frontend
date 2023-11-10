5c04ed44f3916cf5b13ad9c103483de8f1efa689
Sena543 <senanuazanu@gmail.com>
2023-11-10 22:58:35 +0000

    minor changes

--- src/components/booking/About.tsx
+++ src/components/booking/About.tsx
@@ -3,26 +3,25 @@ import welcomeImage from "../../assets/images/welcome-hotel.avif";
 import { Typography } from "@mui/material";
 
 function About() {
-    return (
-        <div className="booking-about-section">
-            <div className="about-div">
-                <div>
-                    <Typography variant="h4">About Hotel</Typography>
-                </div>
-                <div>
-                    <Typography>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus cum
-                        fugit excepturi temporibus nostrum officia debitis autem cumque soluta?
-                        Molestiae nostrum sequi maxime minima quo harum tempora aspernatur deserunt
-                        possimus!
-                    </Typography>
-                </div>
-            </div>
-            <div className="about-image-div">
-                <img src={welcomeImage} loading="lazy" height={"100%"} width={"100%"} />
-            </div>
-        </div>
-    );
+	return (
+		<div className="booking-about-section">
+			<div className="about-div">
+				<div>
+					<Typography variant="h4">About Hotel</Typography>
+				</div>
+				<div>
+					<Typography>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus cum fugit excepturi
+						temporibus nostrum officia debitis autem cumque soluta? Molestiae nostrum sequi maxime minima
+						quo harum tempora aspernatur deserunt possimus!
+					</Typography>
+				</div>
+			</div>
+			<div className="about-image-div">
+				<img src={welcomeImage} loading="lazy" height={"100%"} width={"100%"} />
+			</div>
+		</div>
+	);
 }
 
 export default About;
