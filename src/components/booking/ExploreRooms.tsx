b733c4f1b6a37f0faeabb9e77ab1d62ac71cb97a
Sena543 <senanuazanu@gmail.com>
2023-11-12 20:27:40 +0000

    formatting changes

--- src/components/booking/ExploreRooms.tsx
+++ src/components/booking/ExploreRooms.tsx
@@ -3,55 +3,55 @@ import welcomeImage from "../../assets/images/welcome-hotel.avif";
 import { Typography } from "@mui/material";
 
 const RoomCard = ({ type, description }: { type: string; description: string }) => {
-    return (
-        <div key={description} className="room-card">
-            <div style={{ minHeight: "150px" }} className="room-card-image-div">
-                <img src={welcomeImage} loading="lazy" height={"100%"} width={"100%"} />
-            </div>
-            <div style={{ minHeight: "150px" }}>
-                <Typography>{type}</Typography>
-                <Typography>{description}</Typography>
-            </div>
-        </div>
-    );
+	return (
+		<div key={description} className="room-card">
+			<div style={{ minHeight: "150px" }} className="room-card-image-div">
+				<img src={welcomeImage} loading="lazy" height={"100%"} width={"100%"} />
+			</div>
+			<div style={{ minHeight: "150px" }}>
+				<Typography>{type}</Typography>
+				<Typography>{description}</Typography>
+			</div>
+		</div>
+	);
 };
 
 function ExploreRooms() {
-    const roomsTypes = [
-        {
-            type: "Swimming Pool",
-            description: "1. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
-        },
-        {
-            type: "Gym",
-            description: "2. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
-        },
-        {
-            type: "Luxury & Spa",
-            description: "3. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
-        },
-    ];
-    return (
-        <div className="booking-explore-rooms-section">
-            <div style={{ display: "flex", justifyContent: "flex-start", margin: "2%" }}>
-                <Typography variant="h4">Rooms & Suites</Typography>
-            </div>
+	const roomsTypes = [
+		{
+			type: "Swimming Pool",
+			description: "1. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+		},
+		{
+			type: "Gym",
+			description: "2. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+		},
+		{
+			type: "Luxury & Spa",
+			description: "3. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+		},
+	];
+	return (
+		<div className="booking-explore-rooms-section">
+			<div style={{ display: "flex", justifyContent: "flex-start", margin: "2%" }}>
+				<Typography variant="h4">Rooms & Suites</Typography>
+			</div>
 
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "space-around",
-                    marginTop: "5px",
-                }}
-            >
-                {roomsTypes.map((roomType) => (
-                    <RoomCard type={roomType.type} description={roomType.description} />
-                ))}
-            </div>
-        </div>
-    );
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					alignItems: "center",
+					justifyContent: "space-around",
+					marginTop: "5px",
+				}}
+			>
+				{roomsTypes.map((roomType) => (
+					<RoomCard type={roomType.type} description={roomType.description} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default ExploreRooms;
