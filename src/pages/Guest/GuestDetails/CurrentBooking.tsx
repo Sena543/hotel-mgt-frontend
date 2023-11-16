ba746bcf69e70d952d1f8a698f36415047f80d5a
Sena <senanuazanu@gmail.com>
2023-11-16 13:38:07 +0000

    room details from room not guest

--- src/pages/Guest/GuestDetails/CurrentBooking.tsx
+++ src/pages/Guest/GuestDetails/CurrentBooking.tsx
@@ -2,133 +2,149 @@ import { Icon, IconButton, Typography } from "@mui/material";
 import "./current-booking.css";
 import Carousel from "react-elastic-carousel";
 import {
-	ArrowBackIos,
-	ArrowForwardIos,
-	ArrowForwardIosTwoTone,
-	BedOutlined,
-	CalendarToday,
-	KeyOutlined,
-	Person,
-	Person2Outlined,
-	VpnKeyOutlined,
+    ArrowBackIos,
+    ArrowForwardIos,
+    ArrowForwardIosTwoTone,
+    BedOutlined,
+    CalendarToday,
+    KeyOutlined,
+    Person,
+    Person2Outlined,
+    VpnKeyOutlined,
 } from "@mui/icons-material";
 import CarouselImage from "./CarouselImage";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../../redux/types";
+import { RoomType } from "../../../components/Room/RoomList";
 
 type BookingDetails = {
-	bedType: string;
-	bookingID: string;
-	checkIn: string;
-	checkOut: string;
-	facility: string;
-	guestID: string;
-	mealOrderID: string[];
-	imgUrls: string[];
-	rawDocID: string;
-	roomID: string;
-	roomCapacity?: string;
+    bedType: string;
+    bookingID: string;
+    checkIn: string;
+    checkOut: string;
+    facility: string;
+    guestID: string;
+    mealOrderID: string[];
+    imgUrls: string[];
+    rawDocID: string;
+    roomID: string;
+    roomCapacity?: string;
 };
 
 const breakPoints = [
-	{ width: 1, itemsToShow: 1 },
-	{ width: 550, itemsToShow: 2.5, itemsToScroll: 1 },
-	{ width: 768, itemsToShow: 3.5 },
-	{ width: 1200, itemsToShow: 4.5 },
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2.5, itemsToScroll: 1 },
+    { width: 768, itemsToShow: 3.5 },
+    { width: 1200, itemsToShow: 4.5 },
 ];
 const fontSize = 15;
-function CurrentBooking({ bookingDetails }: { bookingDetails: BookingDetails }) {
-	const dispatch = useDispatch<AppDispatch>();
-	const [currentIndex, setCurrentIndex] = useState(0);
+function CurrentBooking({
+    bookingDetails,
+    roomDetails,
+}: {
+    bookingDetails: BookingDetails;
+    roomDetails: RoomType;
+}) {
+    const dispatch = useDispatch<AppDispatch>();
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-	const next = () => {
-		setCurrentIndex((currentIndex + 1) % bookingDetails.imgUrls.length);
-	};
+    const next = () => {
+        setCurrentIndex((currentIndex + 1) % bookingDetails.imgUrls.length);
+    };
 
-	// move to the previous photo
-	// if we are at the beginning, go to the last photo
-	const prev = () => {
-		setCurrentIndex((currentIndex - 1 + bookingDetails.imgUrls.length) % bookingDetails.imgUrls.length);
-	};
+    // move to the previous photo
+    // if we are at the beginning, go to the last photo
+    const prev = () => {
+        setCurrentIndex(
+            (currentIndex - 1 + bookingDetails.imgUrls.length) % bookingDetails.imgUrls.length
+        );
+    };
 
-	return (
-		<div className="current-booking-container">
-			<div className="booking-history-div">
-				<Typography fontWeight="bold" fontSize="20px">
-					Current Booking
-				</Typography>
-			</div>
-			<Carousel className="carousel-container" itemsToShow={2.3} itemsToScroll={2} isRTL={false}>
-				{/* {bookingDetails?.imgUrls.length > 0
+    console.log(bookingDetails);
+    return (
+        <div className="current-booking-container">
+            <div className="booking-history-div">
+                <Typography fontWeight="bold" fontSize="20px">
+                    Current Booking
+                </Typography>
+            </div>
+            <Carousel
+                className="carousel-container"
+                itemsToShow={2.3}
+                itemsToScroll={2}
+                isRTL={false}
+            >
+                {/* {bookingDetails?.imgUrls.length > 0
 					? bookingDetails?.imgUrls.map((url, index) => (
 							<CarouselImage key={url} imgUrl={url} index={index} />
 					  ))
 					: null} */}
-				{bookingDetails?.imgUrls.map((url, index) => (
-					<CarouselImage key={url} imgUrl={url} index={index} />
-				))}
-			</Carousel>
-			<div>
-				<div className="facilities-div">
-					<Typography>Room Facilities</Typography>
-					<Typography fontSize={14} color="gray">
-						{bookingDetails?.facility}
-					</Typography>
-				</div>
-				<div className="room-details">
-					<div className="room-details">
-						<div
-							style={{
-								backgroundColor: "#79c5f7",
-								borderRadius: "50px",
-								width: "100%",
-								height: "50px",
-								display: "grid",
-								placeItems: "center",
-							}}
-						>
-							<VpnKeyOutlined sx={{ margin: "0.1em", fontSize: 25, color: "white" }} />
-						</div>
-						<div style={{ marginLeft: "5px" }}>
-							<Typography>Booking ID</Typography>
-							<Typography fontSize={fontSize} fontWeight="bolder">
-								#{`${bookingDetails?.bookingID}`}
-							</Typography>
-						</div>
-					</div>
-					<div className="room-details-div">
-						<div className="room-details">
-							<Person2Outlined />
-							<Typography fontSize={fontSize}>Room Capacity</Typography>
-						</div>
-						<Typography fontSize={fontSize + 2} fontWeight="bolder">
-							{/* {bookingDetails.roomCapacity ? bookingDetails.roomCapacity : "N/A"} */}
-							N/A
-						</Typography>
-					</div>
-					<div className="room-details-div">
-						<div className="room-details">
-							<BedOutlined />
-							<Typography fontSize={fontSize}>Bed Type</Typography>
-						</div>
-						<Typography fontSize={fontSize + 2} fontWeight="bolder">
-							{bookingDetails?.bedType}
-						</Typography>
-					</div>
-					<div className="room-details-div">
-						<div className="room-details">
-							<CalendarToday />
-							<Typography fontSize={fontSize}>Booking Date</Typography>
-						</div>
-						<Typography fontSize={fontSize + 2} fontWeight="bolder">
-							Oct 25th - 28th
-						</Typography>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                {roomDetails?.imageUrls.map((url, index) => (
+                    <CarouselImage key={url} imgUrl={url} index={index} />
+                ))}
+            </Carousel>
+            <div>
+                <div className="facilities-div">
+                    <Typography>Room Facilities</Typography>
+                    <Typography fontSize={14} color="gray">
+                        {roomDetails?.facility}
+                    </Typography>
+                </div>
+                <div className="room-details">
+                    <div className="room-details">
+                        <div
+                            style={{
+                                backgroundColor: "#79c5f7",
+                                borderRadius: "50px",
+                                width: "100%",
+                                height: "50px",
+                                display: "grid",
+                                placeItems: "center",
+                            }}
+                        >
+                            <VpnKeyOutlined
+                                sx={{ margin: "0.1em", fontSize: 25, color: "white" }}
+                            />
+                        </div>
+                        <div style={{ marginLeft: "5px" }}>
+                            <Typography>Booking ID</Typography>
+                            <Typography fontSize={fontSize} fontWeight="bolder">
+                                #{`${bookingDetails?.bookingID}`}
+                            </Typography>
+                        </div>
+                    </div>
+                    <div className="room-details-div">
+                        <div className="room-details">
+                            <Person2Outlined />
+                            <Typography fontSize={fontSize}>Room Capacity</Typography>
+                        </div>
+                        <Typography fontSize={fontSize + 2} fontWeight="bolder">
+                            {roomDetails?.roomCapacity ? roomDetails?.roomCapacity : "N/A"}
+                        </Typography>
+                    </div>
+                    <div className="room-details-div">
+                        <div className="room-details">
+                            <BedOutlined />
+                            <Typography fontSize={fontSize}>Bed Type</Typography>
+                        </div>
+                        <Typography fontSize={fontSize + 2} fontWeight="bolder">
+                            {bookingDetails?.bedType}
+                        </Typography>
+                    </div>
+                    <div className="room-details-div">
+                        <div className="room-details">
+                            <CalendarToday />
+                            <Typography fontSize={fontSize}>Booking Date</Typography>
+                        </div>
+                        <Typography fontSize={fontSize + 2} fontWeight="bolder">
+                            Oct 25th - 28th
+                        </Typography>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default CurrentBooking;
