9a1cb9b588cf90274da7b98127c2b1625ce10475
Sena543 <senanuazanu@gmail.com>
2023-11-19 19:31:44 +0000

    header amenities gallery and footer compnents

--- src/pages/Booking/Booking.tsx
+++ src/pages/Booking/Booking.tsx
@@ -4,17 +4,23 @@ import BookingForm from "../../components/booking/BookingForm";
 import About from "../../components/booking/About";
 import ExploreRooms from "../../components/booking/ExploreRooms";
 import Amenities from "../../components/booking/Amenities";
+import Header from "../../components/booking/Header";
+import Gallery from "../../components/booking/Gallery";
+import Footer from "../../components/booking/Footer";
 
 function Booking() {
-    return (
-        <div className="booking-div-container">
-            <Welcome />
-            <BookingForm />
-            <About />
-            <ExploreRooms />
-            <Amenities />
-        </div>
-    );
+	return (
+		<div className="booking-div-container">
+			<Header />
+			<Welcome />
+			<BookingForm />
+			<Amenities />
+			<About />
+			<Gallery />
+			<ExploreRooms />
+			<Footer />
+		</div>
+	);
 }
 
 export default Booking;
