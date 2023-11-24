763d962b8d57eef7cbaec7f20b3c19acd05ce903
Sena <senanuazanu@gmail.com>
2023-11-24 16:30:22 +0000

    working on guest invoice

--- src/pages/Guest/GuestDetails/GuestInvoice.tsx
+++ src/pages/Guest/GuestDetails/GuestInvoice.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./invoice.css";
 import GenericModal from "../../../components/Modal/GenericModal";
 import {
@@ -15,6 +15,13 @@ import {
 } from "@mui/material";
 import { Close } from "@mui/icons-material";
 import { GuestsType } from "../../../constants/genericTypes";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../redux/types";
+import dayjs from "dayjs";
+import { formattedDate } from "../../../utils/util-functions";
+import { fetchTaxeData } from "../../../redux/slices/taxes";
+import { fetchRestaurantMenu } from "../../../redux/slices/restaurantSlice";
+import { fetchAllGuestBookingHistory } from "../../../redux/slices/bookingSlices";
 
 const styles = {
     headerStyle: {
@@ -36,18 +43,78 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 type GuestInvoice = {
     open: boolean;
     setOpen: Function;
-    guestDetails?: GuestsType;
+    guestDetails: GuestsType;
 };
 
 function GuestInvoice({ open, setOpen, guestDetails }: GuestInvoice) {
+    const dispatch = useDispatch<AppDispatch>();
+    const guestRoomStayed = useSelector((state: RootState) =>
+        state.rooms.roomList.filter((room) => {
+            return room.roomName === guestDetails?.roomAssigned;
+        })
+    );
+    const restaurantMeals = useSelector((state: RootState) => state.restaurant.restaurantMealsList);
+    const taxes = useSelector((state: RootState) => state.tax.taxes);
+    const guestBookingDetails = useSelector((state: RootState) =>
+        state.booking.bookingHistory.filter((booking) => {
+            return booking.guestID === guestDetails.guestID;
+        })
+    );
+
+    useEffect(() => {
+        if (taxes.length === 0) {
+            dispatch(fetchTaxeData());
+        }
+        if (restaurantMeals.length === 0) {
+            dispatch(fetchRestaurantMenu());
+        }
+        if (guestBookingDetails.length === 0) {
+            dispatch(fetchAllGuestBookingHistory());
+        }
+    }, [dispatch, taxes]);
+    // console.log({ guestRoomStayed, guestDetails, guestBookingDetails });
     const rows = [
-        { roomNo: "1", des: "lorem ipsum", pricePerNight: "122", otherCharges: "555" },
-        { roomNo: "2", des: "lorem ipsum", pricePerNight: "122", otherCharges: "555" },
+        { roomNo: "1", des: "lorem ipsum", pricePerNight: 122, otherCharges: "555" },
+        { roomNo: "2", des: "lorem ipsum", pricePerNight: 122, otherCharges: "555" },
     ];
 
+    const formatForTable = () => {
+        let arr: (typeof rows)[0][] = [];
+        guestRoomStayed.forEach((room) => {
+            let data = {
+                roomNo: "2",
+                des: "lorem ipsum",
+                pricePerNight: 0,
+                otherCharges: "555",
+                // meals: [],
+            };
+
+            data.roomNo = room.roomName;
+            data.des = room.facility;
+            data.pricePerNight = room.price;
+
+            arr.push(data);
+        });
+
+        return arr;
+    };
+
+    //TODO : FIX THIS ASAP - CHANGE TO USE REDUCE FOR EACH ITEM IN ARRAY
+    const sum = formatForTable().map(
+        (row) =>
+            row.pricePerNight *
+            Number(
+                dayjs(formattedDate(guestDetails.checkOut)).diff(
+                    formattedDate(guestDetails.checkIn),
+                    "day"
+                )
+            )
+    )[0];
+
+    // console.log(formatForTable());
     return (
         // <div className={`invoice-container ${true ? "showInvoice" : null}`}>
-            <div className={`invoice-container ${open ? "showInvoice" : null}`}>
+        <div className={`invoice-container ${open ? "showInvoice" : null}`}>
             <div className="inv-controls">
                 <div></div>
                 <IconButton onClick={() => setOpen(!open)}>
@@ -118,7 +185,7 @@ function GuestInvoice({ open, setOpen, guestDetails }: GuestInvoice) {
                                     <TableCell>
                                         <Typography variant="button">Room</Typography>
                                     </TableCell>
-                                    <TableCell align="right">
+                                    <TableCell align="center">
                                         <Typography variant="button">Description</Typography>
                                     </TableCell>
                                     <TableCell align="right">
@@ -136,33 +203,90 @@ function GuestInvoice({ open, setOpen, guestDetails }: GuestInvoice) {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {rows.map((row) => (
+                                {formatForTable().map((row) => (
+                                    // {rows.map((row) => (
                                     <StyledTableRow key={row.roomNo}>
                                         <TableCell>{row.roomNo}</TableCell>
-                                        <TableCell align="right">{row.des}</TableCell>
-                                        <TableCell align="right">{row.des}</TableCell>
+                                        <TableCell align="center">{row.des}</TableCell>
+                                        <TableCell align="center">
+                                            {/* {dayjs(formattedDate(guestDetails.checkOut))} */}
+                                            {dayjs(formattedDate(guestDetails.checkOut)).diff(
+                                                formattedDate(guestDetails.checkIn),
+                                                "day"
+                                            )}
+                                        </TableCell>
                                         <TableCell align="right">{row.pricePerNight}</TableCell>
                                         <TableCell align="right">{row.otherCharges}</TableCell>
-                                        <TableCell align="right">{123}</TableCell>
+                                        <TableCell align="right">
+                                            {row.pricePerNight *
+                                                Number(
+                                                    dayjs(
+                                                        formattedDate(guestDetails.checkOut)
+                                                    ).diff(
+                                                        formattedDate(guestDetails.checkIn),
+                                                        "day"
+                                                    )
+                                                )}
+                                        </TableCell>
                                     </StyledTableRow>
                                 ))}
                                 <TableRow>
                                     <TableCell rowSpan={3} />
                                     <TableCell colSpan={4}>Subtotal</TableCell>
                                     <TableCell align="right">
-                                        <Typography>123</Typography>
+                                        <Typography>
+                                            {formatForTable().map(
+                                                (row) =>
+                                                    row.pricePerNight *
+                                                    Number(
+                                                        dayjs(
+                                                            formattedDate(guestDetails.checkOut)
+                                                        ).diff(
+                                                            formattedDate(guestDetails.checkIn),
+                                                            "day"
+                                                        )
+                                                    )
+                                            )}
+                                        </Typography>
                                         {/* <Typography>{invoiceSubtotal}</Typography> */}
                                     </TableCell>
                                 </TableRow>
                                 <TableRow>
                                     <TableCell colSpan={3}>Tax</TableCell>
-                                    <TableCell align="right">
-                                        <Typography>
-                                            {`${(0.07 * 100).toFixed(0)} %`}
-                                            {/* {`${(TAX_RATE * 100).toFixed(0)} %`} */}
-                                        </Typography>
+                                    <TableCell>
+                                        <TableRow>
+                                            {taxes.map((tax) => (
+                                                <div
+                                                    key={tax.rawDocID}
+                                                    style={{
+                                                        display: "flex",
+                                                        justifyContent: "space-between",
+                                                    }}
+                                                >
+                                                    <Typography style={{ marginRight: "3px" }}>
+                                                        {tax.name}
+                                                    </Typography>{" "}
+                                                    <Typography>{tax.value * 100}%</Typography>
+                                                </div>
+                                            ))}
+                                        </TableRow>
                                     </TableCell>
-                                    <TableCell align="right">{123}</TableCell>
+                                    {/* <TableCell align="right">{123}</TableCell> */}
+                                    <TableCell align="left">
+                                        <TableRow>
+                                            {taxes.map((tax) => (
+                                                <div
+                                                    key={tax.rawDocID}
+                                                    style={{
+                                                        display: "flex",
+                                                        justifyContent: "space-between",
+                                                    }}
+                                                >
+                                                    <Typography>{tax.value * sum}</Typography>
+                                                </div>
+                                            ))}
+                                        </TableRow>
+                                    </TableCell>
                                 </TableRow>
                                 <TableRow style={{ backgroundColor: "rgba(128, 82, 157, 0.08)" }}>
                                     <TableCell colSpan={4}>Total</TableCell>
@@ -177,4 +301,7 @@ function GuestInvoice({ open, setOpen, guestDetails }: GuestInvoice) {
     );
 }
 
+{
+    /* {`${(0.07 * 100).toFixed(0)} %`} */
+}
 export default GuestInvoice;
