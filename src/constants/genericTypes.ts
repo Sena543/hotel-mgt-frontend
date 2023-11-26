a60e086eee7a472559098b8fc3219cf453ba7130
Sena543 <senanuazanu@gmail.com>
2023-11-26 21:51:03 +0000

    dbitem type

--- src/constants/genericTypes.ts
+++ src/constants/genericTypes.ts
@@ -1,50 +1,62 @@
 export type GuestsType = {
-    lastName: string;
-    firstName: string;
-    roomAssigned: string;
-    checkIn: string | any;
-    checkOut: string | any;
-    specialRequests?: string;
-    status?: string;
-    email: string;
-    contact: string;
-    guestID: Number;
+	lastName: string;
+	firstName: string;
+	roomAssigned: string;
+	checkIn: string | any;
+	checkOut: string | any;
+	specialRequests?: string;
+	status?: string;
+	email: string;
+	contact: string;
+	guestID: Number;
 };
 
 export type StaffDetailsType = {
-    lastName: string;
-    firstName: string;
-    email: string;
-    contact: string;
-    jobDescription: string;
-    jobTitle: string;
-    workingDays: string[];
-    employeeID: string;
+	lastName: string;
+	firstName: string;
+	email: string;
+	contact: string;
+	jobDescription: string;
+	jobTitle: string;
+	workingDays: string[];
+	employeeID: string;
 };
 
 export type MenuItemType = {
-    rawDocID?: string;
-    menuType: string;
-    dishOrBev: string;
-    price: string;
-    dishType: string;
-    description: string;
+	rawDocID?: string;
+	dishID: Number;
+	menuType: string;
+	dishOrBev: string;
+	price: string;
+	dishType: string;
+	description: string;
+};
+
+type DBMenuItemType = {
+	beverageID: Number;
+	beveragePrice: Number;
+	guestID: Number;
+	guestName: String;
+	mealID: Number;
+	mealPrice: Number;
+	roomId: String;
 };
 
 export type BookingHistoryType = {
-    bookingID: string;
-    guestID: Number;
-    // guestID: string;
-    roomID: string;
-    checkIn: string;
-    checkOut: string;
-    // mealOrderID?: string[];
-    mealOrderID?: MenuItemType[];
-    rawDocID?: string;
+	bookingID: string;
+	guestID: Number;
+	// guestID: string;
+	roomID: string;
+	checkIn: string;
+	checkOut: string;
+	// mealOrderID?: string[];
+	mealOrderID: DBMenuItemType[];
+	// mealOrderID?: MenuItemType[];
+	rawDocID?: string;
 };
 
 export type TaxInformation = {
-    rawDocID?: string;
-    name: string;
-    value: number;
+	rawDocID?: string;
+	name: string;
+	value: number;
 };
