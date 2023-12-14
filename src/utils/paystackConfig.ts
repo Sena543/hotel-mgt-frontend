3b08fec57070559ceab963e69c339036717671c4
Sena <senanuazanu@gmail.com>
2023-12-14 14:59:27 +0000

    paystack cofing complete

--- src/utils/paystackConfig.ts
+++ src/utils/paystackConfig.ts
@@ -1,10 +1,24 @@
+import { PaystackProps } from "react-paystack/dist/types";
 import { GuestsType } from "../constants/genericTypes";
 
-export default function paystackConfig(profileDetails: GuestsType) {
+export function onSucess(reference: string) {
+    //TODO- function to save the reference to be
+    console.log(reference);
+}
+
+export function onClose() {}
+export default function paystackConfig(
+    profileDetails: GuestsType,
+    totalAmount: number
+): PaystackProps {
     return {
-        // reference: new Date().getTime().toString(),
+        reference: new Date().getTime().toString(),
         email: profileDetails?.email,
-        amount: 20000, //Amount is in the country's lowest currency. E.g Kobo, so 20000 kobo = N200
+        amount: totalAmount * 100, //Amount is in the country's lowest currency. E.g Kobo, so 20000 kobo = N200
         publicKey: import.meta.env.VITE_PAYSTACK_PUBLIC_KEY,
+        lastname: profileDetails.lastName,
+        firstname: profileDetails.firstName,
+        phone: profileDetails.contact,
+        currency: "GHS",
     };
 }
