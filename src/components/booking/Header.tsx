2d03cde3f62d25b7d30bc68003267606211832b1
senanu <senanuazanu@gmail.com>
2024-07-18 21:48:04 +0000

    logo

--- src/components/booking/Header.tsx
+++ src/components/booking/Header.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import LogoImg from "../../assets/images/logo.jpg";
+// import LogoImg from "../../../public/assets/images/logo.jpg";
 
 function Header() {
 	const links = ["Home", "About", "Teams", "Gallery", "Rooms", "Contact us"];
 	return (
 		<div className="booking-header-container">
-			<div>logo</div>
+			{/* <div>logo</div> */}
+			<img src={LogoImg} loading="lazy" height={"100%"} width={"12%"} />
 			<div className="links-div" style={{}}>
 				{links &&
 					links.map((link) => (
